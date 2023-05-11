32ee68db298a9e7a122436c81bbfe645581aebee
https://gitlab.fokus.fraunhofer.de/sbo <sebastian.bock@fokus.fraunhofer.de>
2023-05-11 10:31:59 +0200

    license update to add secondary license option

--- src/qrisp/interface/openapi_interface/codegen/client/openapi_client/api/__init__.py
+++ src/qrisp/interface/openapi_interface/codegen/client/openapi_client/api/__init__.py
@@ -1,3 +1,21 @@
+"""
+/********************************************************************************
+* Copyright (c) 2023 the Qrisp authors
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* This Source Code may also be made available under the following Secondary
+* Licenses when the conditions for such availability set forth in the Eclipse
+* Public License, v. 2.0 are satisfied: GNU General Public License, version 2 
+* or later with the GNU Classpath Exception which is
+* available at https://www.gnu.org/software/classpath/license.html.
+*
+* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0
+********************************************************************************/
+"""
+
 # do not import all apis into this module because that uses a lot of memory and stack frames
 # if you need the ability to import all apis from one package, import them with
 # from openapi_client.apis import DefaultApi
