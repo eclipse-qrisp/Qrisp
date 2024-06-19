2b5e4993b4d62d49f2f702017afc2ccdce458eac
positr0nium <raphaelsdel@gmail.com>
2024-06-19 12:06:58 +0200

    better expression treatment in gray synth gate objects

--- src/qrisp/logic_synthesis/gray_synthesis.py
+++ src/qrisp/logic_synthesis/gray_synthesis.py
@@ -17,6 +17,8 @@
 """
 
 import numpy as np
+import sympy as sp
+
 from qrisp.misc import array_as_int, gate_wrap, int_as_array
 from qrisp.circuit import QuantumCircuit, Operation, PTControlledOperation, ControlledOperation, fast_append
 
@@ -433,7 +435,7 @@ class GraySynthGate(Operation):
         
         self.abstract_params = set()
         for i in range(len(target_phases)):
-            if not isinstance(target_phases[i], (int, float, np.float64)):
+            if isinstance(target_phases[i], sp.Expr):
                 self.abstract_params = self.abstract_params.union(target_phases[i].free_symbols)
         
     def control(self, num_ctrl_qubits=1, ctrl_state=-1, method=None):
