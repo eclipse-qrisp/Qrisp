918f3e3751094256315572d925e45295fd928fa3
positr0nium <raphaelsdel@gmail.com>
2024-08-29 13:49:50 +0200

    removed depedencies from deprecated interface code

--- setup.py
+++ setup.py
@@ -18,11 +18,8 @@
 
 import setuptools
 
-REQUIREMENTS = ["connexion>=2.12.0",
-                "qiskit>=0.44.0",
-                "thrift>=0.15.0",
+REQUIREMENTS = ["qiskit>=0.44.0",
                 "matplotlib>=3.5.1",
-                "waitress>=2.1.1",
                 "scipy>=1.10.0",
                 "numba",
                 "networkx",
