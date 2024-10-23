c25597a7a93ad5b7c0e852f56d5c30c79c29b91c
Steinmann <niklas.steinmann@fokus.fraunhofer.de>
2024-10-23 14:46:45 +0200

    updated QIRO tests

--- tests/test_QIROmaxClique.py
+++ tests/test_QIROmaxClique.py
@@ -17,8 +17,8 @@
 """
 
 from qrisp import QuantumVariable
-from qrisp.qiro import QIROProblem, create_max_clique_replacement_routine, create_max_clique_cost_operator_reduced, qiro_RXMixer, qiro_init_function
-from qrisp.qaoa import max_clique_problem, create_max_clique_cl_cost_function
+from qrisp.algorithms.qiro import QIROProblem, create_max_clique_replacement_routine, create_max_clique_cost_operator_reduced, qiro_rx_mixer, qiro_init_function
+from qrisp.algorithms.qaoa import max_clique_problem, create_max_clique_cl_cost_function
 import networkx as nx
 
 def test_qiro_max_clique():
@@ -34,7 +34,7 @@ def test_qiro_max_clique():
     qiro_instance = QIROProblem(problem = G,
                                 replacement_routine = create_max_clique_replacement_routine,
                                 cost_operator = create_max_clique_cost_operator_reduced,
-                                mixer = qiro_RXMixer,
+                                mixer = qiro_rx_mixer,
                                 cl_cost_function = create_max_clique_cl_cost_function,
                                 init_function = qiro_init_function
                                 )
