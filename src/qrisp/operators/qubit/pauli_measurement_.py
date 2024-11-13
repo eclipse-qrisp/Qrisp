3b83669619e9a3a34e6972b03c89f8a64a1be8d2
positr0nium <raphaelsdel@gmail.com>
2024-11-13 13:06:12 +0100

    restructured such that a singular vqe call doesn't construct measurement data multiple times

--- src/qrisp/operators/qubit/pauli_measurement_.py
+++ src/qrisp/operators/qubit/pauli_measurement_.py
@@ -16,151 +16,193 @@
 ********************************************************************************/
 """
 
-from qrisp import QuantumVariable, QuantumArray, QuantumCircuit
 import numpy as np
+from numba import njit
 
-
-def get_integer_from_indices(indices,positions=None):
-    if positions is not None:
-        return sum(1 << positions[i] for i in indices)
-    else:
-        return sum(1 << i for i in indices)
-
-
-class PauliMeasurement:
-    """
+class QubitOperatorMeasurement:
     
-    
-    """
-
-    def __init__(self, operator):
-        """
-
-        Parameters
-        ----------
-        operator: QubitHamiltonian or BoundQubitHamiltonian 
+    def __init__(self, qubit_op):
         
-        Attributes
-        ----------
-        bases : list[QubitTerm]
-            The basis of each group as QubitTerm.
-        operators_ind : list[list[list[int]]]
-            The QubitTerms in each group as list of integers. 
-            The integers correspond to the positions of "Z" in the QubitTerm (after change of basis).
-        operators_int : list[list[int]]
-            The QubitTerms in each group as integers. 
-            A "1" at position j in binary representation corresponds to a "Z" at position j in the QubitTerm (after change of basis).
-        coefficients : list[list[float]]
-            The coeffcients of the QubitTerms in each group.
-        circuits : list[QuantumCircuit]
-            The change of basis circuits for each group.
-        qubits : list[list[int or Qubit]]
-            The qubits to be measured.
-        variances : list[float]
-            The variances for the groups.
-        shots : list[float]
-            The optimal distribution of shots among the groups.
-
-        """
-        self.bases, self.operators_ind, self.operators_int, self.coefficients = self.commuting_qw_measurement(operator)
-        self.variances, self.shots = self.measurement_shots()
-        self.circuits = self.measurement_circuits()
-
-    def commuting_qw_measurement(self, operator):
-        """
+        hamiltonian = qubit_op.hermitize()
+        hamiltonian = hamiltonian.eliminate_ladder_conjugates()
         
-        """
-
-        groups, bases = operator.commuting_qw_groups(show_bases=True)
-        self.groups = groups
-        operators_ind = []
-        operators_int = []
-        coefficients = []
-
-        # List of dictionaries with qubits in basis as keys and their position in an ordered list as values
-        positions = []
-        for basis in bases:
-            ordered_keys = sorted(basis.factor_dict.keys())
-            position_dict = {key: index for index, key in enumerate(ordered_keys)}
-            positions.append(position_dict)
-
-        n = len(groups)
-        for i in range(n):
-            curr_ind = []
-            curr_int = []
-            curr_coeff = []
-
-            for term,coeff in groups[i].terms_dict.items():
-                ind = list(term.factor_dict.keys())
-
-                curr_ind.append(ind)
-                curr_int.append(term.serialize())
-                curr_coeff.append(float(coeff.real))
-
-            operators_ind.append(curr_ind)
-            operators_int.append(curr_int)
-            coefficients.append(curr_coeff)
-        return bases, operators_ind, operators_int, coefficients
+        self.groups = hamiltonian.commuting_qw_groups()
         
-
-    def measurement_shots(self):
-        """
-        Calculates the optimal distribution and number of shots following https://quantum-journal.org/papers/q-2021-01-20-385/pdf/.
+        self.n = len(self.groups)
         
-        """
-        n = len(self.coefficients)
-        variances = []
-        for i in range(n):
-            m = len(self.coefficients[i])
+        # Collect standard deviation
+        self.stds = []
+        for group in self.groups:
+            self.stds.append(np.sqrt(group.to_pauli().hermitize().get_operator_variance()))
             
-            var = sum(self.coefficients[i][j]**2 for j in range(m) if (self.operators_int[i][j][0]+self.operators_int[i][j][1])>0) # Exclude constant term
-            variances.append(var)
-        N = sum(np.sqrt(x) for x in variances)
-        shots = [np.sqrt(x)*N for x in variances]
-        return variances, shots
-
-    def measurement_circuits(self):
-        """
-        Constructs the change of basis circuits for the QWC method.
+        N = sum(self.stds)
+        self.shots_list = [N*s for s in self.stds]
         
-        """
-
-        circuits = []
-
-        # Construct change of basis circuits
-        for i in range(len(self.bases)):
+        self.conjugation_circuits = []
+        self.measurement_operators = []
+        
+        for group in self.groups:
+            
+            conj_circuit, meas_op = group.get_conjugation_circuit()
+            
+            self.conjugation_circuits.append(conj_circuit)
+            self.measurement_operators.append(meas_op)
+            
+    
+    def get_measurement(self, qc, qubit_list, precision, backend):
+        
+        from qrisp.misc import get_measurement_from_qc
+        results = []
+        meas_ops = []
+        meas_coeffs = []
+        
+        for i in range(self.n):
             
             group = self.groups[i]
-            n = group.find_minimal_qubit_amount()
-            qc = QuantumCircuit(n)
+            conjugation_circuit = self.conjugation_circuits[i]
+            shots = int(self.shots_list[i]/precision**2)
             
-            factor_dict = self.bases[i].factor_dict
-            for j in range(n):
-                if factor_dict.get(j, "I")=="X":
-                    qc.h(j)
-                if factor_dict.get(j, "I")=="Y":
-                    qc.sx(j)
-                    
-            for term in self.groups[i].terms_dict.keys():
+            curr = qc.copy()
+            qubits = [qubit_list[i] for i in range(conjugation_circuit.num_qubits())]
+            curr.append(conjugation_circuit.to_gate(), qubits)
+            
+            res = get_measurement_from_qc(curr.transpile(), list(qubit_list), backend, shots)
+            results.append(res)
+            
+            temp_meas_ops = []
+            temp_coeff = []
+            for term, coeff in group.terms_dict.items():
+                temp_meas_ops.append(term.serialize())
+                temp_coeff.append(coeff)
                 
-                ladder_operators = [base for base in term.factor_dict.items() if base[1] in ["A", "C"]]
-                
-                if len(ladder_operators):
-                    anchor_factor = ladder_operators[-1]
-                
-                    if anchor_factor[1] == "A":
-                        qc.x(anchor_factor[0])
-                        
-                for j in range(len(ladder_operators)-1):
-                    qc.cx(anchor_factor[0], ladder_operators[j][0])
+            meas_coeffs.append(temp_coeff)
+            meas_ops.append(temp_meas_ops)
+            
+        
+        samples = create_padded_array([list(res.keys()) for res in results]).astype(np.int64)
+        probs = create_padded_array([list(res.values()) for res in results])
+        meas_ops = create_padded_array(meas_ops, use_tuples = True).astype(np.int64)
+        meas_coeffs = create_padded_array(meas_coeffs)
+        
+        return evaluate_expectation_jitted(samples, probs, meas_ops, meas_coeffs)
+        
 
-                if len(ladder_operators):
-                    if anchor_factor[1] == "A":
-                        qc.x(anchor_factor[0])
-                
-                    qc.h(anchor_factor[0])
 
-            circuits.append(qc)    
-
-        return circuits
+def create_padded_array(list_of_lists, use_tuples = False):
+    """
+    Create a padded numpy array from a list of lists with varying lengths.
     
+    Parameters:
+    list_of_lists (list): A list of lists with potentially different lengths.
+    
+    Returns:
+    numpy.ndarray: A 2D numpy array with padded rows.
+    """
+    # Find the maximum length of any list in the input
+    max_length = max(len(lst) for lst in list_of_lists)
+    
+    # Create a padded list of lists
+    if not use_tuples:
+        padded_lists = [
+            lst + [0] * (max_length - len(lst))
+            for lst in list_of_lists
+        ]
+    else:
+        padded_lists = [
+            lst + [(0,0,0,0)] * (max_length - len(lst))
+            for lst in list_of_lists
+        ]
+        
+    
+    # Convert to numpy array
+    return np.array(padded_lists)
+
+
+#
+# Evaluate expectation
+#
+
+
+def evaluate_expectation(samples, probs, operators, coefficients):
+    """
+    Evaluate the expectation.
+    
+    """
+    # print(results)
+    # print(operators)
+    # print(coefficients)
+    # raise
+
+    expectation = 0
+
+    for index1,ops in enumerate(operators):
+        for index2,op in enumerate(ops):
+            for i in range(len(samples[index1])):
+                outcome,probability = samples[index1, i], probs[index1, i]
+                expectation += probability*evaluate_observable(op,outcome)*np.real(coefficients[index1][index2])
+    
+    return expectation
+
+
+def evaluate_observable(observable: tuple, x: int):
+    # This function evaluates how to compute the energy of a measurement sample x.
+    # Since we are also considering ladder operators, this energy can either be
+    # 0, -1 or 1. For more details check out the comments of QubitOperator.get_conjugation_circuit
+    
+    # The observable is given as tuple, containing for integers and a boolean.
+    # To understand the meaning of these integers check QubitTerm.serialize.
+    
+    # Unwrap the tuple
+    z_int, AND_bits, AND_ctrl_state, contains_ladder = observable
+
+    # Compute whether the sign should be sign flipped based on the Z operators
+    sign_flip_int = z_int & x
+    sign_flip = 0
+    while sign_flip_int:
+        sign_flip += sign_flip_int & 1
+        sign_flip_int >>= 1
+    
+    # If there is a ladder operator in the term, we need to half the energy 
+    # because we want to measure (|110><110| - |111><111|)/2
+    if contains_ladder:
+        prefactor = 0.5
+    else:
+        prefactor = 1
+    
+    # If there are no and bits, we return the result
+    if AND_bits == 0:
+        return prefactor*(-1)**sign_flip
+
+    # Otherwise we apply the AND_ctrl_state to flip the appropriate bits.
+    corrected_x = (x ^ AND_ctrl_state)
+    
+    # If all bits are in the 0 state the AND is true.
+    if corrected_x & AND_bits == 0:
+        return prefactor*(-1)**sign_flip
+    else:
+        return 0
+    
+
+evaluate_observable_jitted = njit(cache = True)(evaluate_observable)
+
+@njit(cache = True)
+def evaluate_expectation_jitted(samples, probs, operators, coefficients):
+    """
+    Evaluate the expectation.
+    
+    """
+    # print(results)
+    # print(operators)
+    # print(coefficients)
+    # raise
+
+    expectation = 0
+
+    for index1,ops in enumerate(operators):
+        for index2,op in enumerate(ops):
+            for i in range(len(samples[index1])):
+                outcome,probability = samples[index1, i], probs[index1, i]
+                expectation += probability*evaluate_observable_jitted(op,outcome)*np.real(coefficients[index1][index2])
+    
+    return expectation
+
