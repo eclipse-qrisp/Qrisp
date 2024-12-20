ead532a04cc1de44df87a67c98e7ca9c5f25a297
Darya Martyniuk <darya.martyniuk@fokus.fraunhofer.de>
2024-12-20 12:31:58 +0100

    refactoring for the BackendServer - formatting, typos

--- src/qrisp/interface/qunicorn/backend_server.py
+++ src/qrisp/interface/qunicorn/backend_server.py
@@ -17,7 +17,6 @@
 """
 
 from flask import Flask, request, jsonify, make_response
-import yaml
 from datetime import date, datetime
 import threading
 
@@ -34,6 +33,7 @@ https://qunicorn-core.readthedocs.io/en/latest/tutorials/pilot_tutorial_demo.htm
 
 """
 
+
 # Returns the hosts ip
 def get_ip():
     import socket
@@ -43,17 +43,18 @@ def get_ip():
     try:
         # doesn't even have to be reachable
         s.connect(("10.255.255.255", 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = "127.0.0.1"
+        ip = s.getsockname()[0]
+    except (socket.error, socket.gaierror, OSError):
+        ip = "127.0.0.1"
     finally:
         s.close()
-    return IP
+    return ip
 
 
 class BackendServer:
     """
-    This class allows convenient setup of a server respecting the `Qunicorn<https://qunicorn-core.readthedocs.io/en/latest/index.html>`_ interface.
+    This class allows convenient setup of a server respecting the
+    `Qunicorn<https://qunicorn-core.readthedocs.io/en/latest/index.html>`_ interface.
 
     Parameters
     ----------
@@ -66,9 +67,6 @@ class BackendServer:
         IP address of the hosting machine will be used.
     port : int, optional
         The port on which to listen for requests. By default, 9010 will be used.
-    is_simulator : bool, optional
-        A bool specifying whether the results returned by this server are due to a
-        simulator. The default is True.
 
     Examples
     --------
@@ -77,10 +75,10 @@ class BackendServer:
     prints the token and queries the QASM-simulator. ::
 
         def run_func(qasm_str, shots):
-            
+
             from qiskit import QuantumCircuit
             #Convert to qiskit
-            
+
             qiskit_qc = QuantumCircuit.from_qasm_str(qasm_str)
 
             from qiskit import Aer
@@ -94,8 +92,8 @@ class BackendServer:
         example_server.start()
 
     """
-    
-    def __init__(self, run_func , ip_address = None, port = None):
+
+    def __init__(self, run_func, ip_address=None, port=None):
 
         self.app = Flask(__name__)
         self.deployments = []
@@ -104,200 +102,184 @@ class BackendServer:
         if port is None:
             port = 9010
         self.port = port
-        self.ip_address = ip_address 
+        self.ip_address = ip_address
         self.jobs = []
         self.run_func = run_func
         self.is_simulator = True
+
         # To set up the server we route the relevant functions for the flask app.
-        
-        #This function receives the deployment data and saves it in the 
-        #deployment attribute
+
+        # This function receives the deployment data and saves it in the
+        # deployment attribute
         @self.app.route('/deployments', methods=['POST'])
         def deployments():
-            
+
             # Receive the quantum circuit data from the request
             data = request.get_json()
-            
-            #Append the deployment data to the library
+
+            # Append the deployment data to the library
             self.deployments.append(data)
-                
-            #For the response dictionary
-            response = {
-                      "id": len(self.deployments)-1,
-                      "deployedBy": {
-                        "id": 0,
-                        "name": ""
-                      },
-                      "deployedAt": str(date.today()),
-                      "name": ""
-                    }
-            
-            response["programs"] = data["programs"]
-            
-            
-            #Create response object
-            response_object = make_response(jsonify(response), 201) 
+
+            # For the response dictionary
+            response = {"id": len(self.deployments) - 1, "deployedBy": {
+                "id": 0,
+                "name": ""
+            }, "deployedAt": str(date.today()), "name": "", "programs": data["programs"]}
+
+            # Create response object
+            response_object = make_response(jsonify(response), 201)
             return response_object
-        
-        #This function receives the deployment id of a job and starts the execution
+
+        # This function receives the deployment id of a job and starts the execution
         @self.app.route('/jobs', methods=['POST'])
         def jobs_post():
-            
+
             # Receive the quantum circuit data from the request
             data = request.get_json()
-            
-            #Extract the deployment
+
+            # Extract the deployment
             deployment_id = data["deploymentId"]
             deployment = self.deployments[deployment_id]
-            
-            #Create the wrapper for asynchronous execution of the job
+
+            # Create the wrapper for asynchronous execution of the job
             def job_wrapper():
                 for i in range(len(deployment["programs"])):
-                    counts_res = self.run_func(deployment["programs"][i]["quantumCircuit"], 
-                                                data["shots"], 
-                                                data["token"])
+                    counts_res = self.run_func(deployment["programs"][i]["quantumCircuit"],
+                                               data["shots"],
+                                               data["token"])
                     job_dic["counts"].append(counts_res)
                 # try:
                 #     for i in range(len(deployment["programs"])):
-                #         counts_res = self.run_func(deployment["programs"][i]["quantumCircuit"], 
-                #                                     data["shots"], 
+                #         counts_res = self.run_func(deployment["programs"][i]["quantumCircuit"],
+                #                                     data["shots"],
                 #                                     data["token"])
                 #         job_dic["counts"].append(counts_res)
                 # except Exception as e:
                 #     job_dic["exception"] = e
-                    
+
                 job_dic["end_time"] = str(datetime.now())
-            
-            #Create the thread
-            run_thread = threading.Thread(target = job_wrapper)    
-            
-            #Create the dictionary to hold information about the job 
-            job_dic = {}
-            
-            job_dic["deploymentId"] = deployment_id
-            job_dic["counts"] = []
-            job_dic["shots"] = data["shots"]
-            job_dic["exception"] = None
-            job_dic["quantumCircuits"] = deployment["programs"]
-            job_dic["run_thread"] = run_thread
-            job_dic["start_time"] = str(datetime.now())
-            job_dic["end_time"] = "-"
-            
-            
-            #Start the thread            
+
+            # Create the thread
+            run_thread = threading.Thread(target=job_wrapper)
+
+            # Create the dictionary to hold information about the job
+            job_dic = {"deploymentId": deployment_id, "counts": [], "shots": data["shots"], "exception": None,
+                       "quantumCircuits": deployment["programs"], "run_thread": run_thread,
+                       "start_time": str(datetime.now()), "end_time": "-"}
+
+            # Start the thread
             run_thread.start()
-            
-            #Append the job to jobs attribute            
+
+            # Append the job to jobs attribute
             self.jobs.append(job_dic)
-            
-            #Form the response
+
+            # Form the response
             response = {
-                    "id": len(self.jobs)-1,
-                    "jobName": "",
-                    "jobState": "running"
-                    }
-            response_object = make_response(jsonify(response), 201) 
-            
+                "id": len(self.jobs) - 1,
+                "jobName": "",
+                "jobState": "running"
+            }
+            response_object = make_response(jsonify(response), 201)
+
             return response_object
-        
-        #This function retrieves information about a certain job. It is used by
-        #the client to inquire the results of the job
+
+        # This function retrieves information about a certain job. It is used by
+        # the client to inquire the results of the job
         @self.app.route('/jobs/<int:job_id>/', methods=['GET'])
         def get_job(job_id):
-            
+
             # Search for the job with the specified job_id
             if not job_id < len(self.jobs):
                 return jsonify({"error": "Job not found"}), 404
-            
-            #Get job information
+
+            # Get job information
             job = self.jobs[job_id]
-            
-            #Find out wether the job finished
+
+            # Find out weather the job finished
             thread = job["run_thread"]
-            
+
             if not thread.is_alive():
                 state = "FINISHED"
             else:
                 state = "RUNNING"
-            
-            #Find out wether the job suceeded
+
+            # Find out wether the job succeeded
             if job["exception"] is not None:
-                
                 state = "failed"
                 response = {
-                  "code": 0,
-                  "status": "failed",
-                  "message": str(job["exception"]),
-                  "errors": {}
+                    "code": 0,
+                    "status": "failed",
+                    "message": str(job["exception"]),
+                    "errors": {}
                 }
-                
-                response_object = make_response(jsonify(response)) 
+
+                response_object = make_response(jsonify(response))
                 return response_object
-            
-            #Form the response
+
+            # Form the response
             response = {
-              "id": job_id,
-              "executedBy": {
-                "id": 0,
-                "name": ""
-              },
-              "executedOn": {
-                "id": 0,
-                "numQubits": -1,
-                "isSimulator": self.is_simulator,
-                "isLocal": True,
-                "provider": {
-                  "id": 0,
-                  "withToken": True,
-                  "supportedLanguages": {
+                "id": job_id,
+                "executedBy": {
                     "id": 0,
-                    "providerId": "string",
                     "name": ""
-                  },
-                  "name": ""
-                }
-              },
-              "progress": 0,
-              "state": state,
-              "type": "",
-              "startedAt": job["start_time"],
-              "finishedAt": job["end_time"],
-              "data": "",
-              "results": [{
-                "id": 0,
-                "circuit": job["quantumCircuits"][i],
-                "data": job["counts"][i],
-                "resultType": "COUNTS",
-                "metadata": {"format" : "bin"},
-              } for i in range(len(job["counts"]))],
-              "parameters": ""
+                },
+                "executedOn": {
+                    "id": 0,
+                    "numQubits": -1,
+                    "isSimulator": self.is_simulator,
+                    "isLocal": True,
+                    "provider": {
+                        "id": 0,
+                        "withToken": True,
+                        "supportedLanguages": {
+                            "id": 0,
+                            "providerId": "string",
+                            "name": ""
+                        },
+                        "name": ""
+                    }
+                },
+                "progress": 0,
+                "state": state,
+                "type": "",
+                "startedAt": job["start_time"],
+                "finishedAt": job["end_time"],
+                "data": "",
+                "results": [{
+                    "id": 0,
+                    "circuit": job["quantumCircuits"][i],
+                    "data": job["counts"][i],
+                    "resultType": "COUNTS",
+                    "metadata": {"format": "bin"},
+                } for i in range(len(job["counts"]))],
+                "parameters": ""
             }
-    
+
             response_object = make_response(jsonify(response), 200)
-            
+
             return response_object
 
     def start(self):
-        
+
         from waitress import serve
-        
-        #Set up the thread to run the server    
+
+        # Set up the thread to run the server
         def wrapper():
             serve(self.app, host=self.ip_address, port=self.port)
             # self.app.run(host=self.ip_address, port=self.port)
+
         thr = threading.Thread(target=wrapper)
         thr.setDaemon(True)
 
         # Start the thread
         thr.start()
-        
+
         import requests
-        # Hold programm until the server answers
+        # Hold program until the server answers
         while True:
             try:
-                response = requests.get("http://" + self.ip_address + ":" + str(self.port) + "/jobs/0/")
-            except:
+                requests.get("http://" + self.ip_address + ":" + str(self.port) + "/jobs/0/")
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed: {e}")
                 continue
             break
-            
-            
