f6fdcbd60f195a34ac1bea303521587a94a5a035
positr0nium <raphaelsdel@gmail.com>
2025-01-23 14:09:59 +0100

    wrote some comments for the sampling implementation

--- src/qrisp/jasp/program_control/sampling.py
+++ src/qrisp/jasp/program_control/sampling.py
@@ -21,6 +21,32 @@ import jax.numpy as jnp
 
 from qrisp.jasp.tracing_logic import quantum_kernel, check_for_tracing_mode
 
+# The following function implements the sample feature.
+
+# The basic functionality would be relatively straightforward to implement,
+# however there are some complications. The reason for that is that the resulting
+# jaxpr should be "readable" by the terminal sampling interpreter.
+# Terminal sampling means that instead of performing the simulations "shots"-times
+# it is performed once and the shots are then sampled from that distribution.
+# Naturally this implies a massive performance increase, which is why a lot
+# of effort is spent to realize a smooth implementation.
+
+# The underlying idea to make the feature easily "readable" by the terminal
+# sampling interpreter is to structure one iteration of sampling into three
+# steps.
+
+# 1. Evaluating the user function, which generates the distribution.
+# 2. Sampling from that distribution via the "measure" function.
+# 3. Decoding and postprocessing the measurement results.
+
+# For the final two steps we deploy some custom logic to realize the terminal
+# sampling behavior. To simplify the automatic processing of these steps,
+# we capture each into individual pjit calls.
+
+# The terminal sampling interpreter then identifies each steps via the
+# eqn.params["name"] attribute and executes the custom logic.
+
+
 def sample(func = None, shots = 0, post_processor = None):
     
     from qrisp.jasp import qache
@@ -46,25 +72,31 @@ def sample(func = None, shots = 0, post_processor = None):
     elif not isinstance(shots, int):
         raise Exception(f"Tried to sample with shots value of non-integer type {type(shots)}")
     
+    # Qache the user function
     @qache
     def user_func(*args):
         return func(*args)
 
+    # This function evaluates the sampling process
     @jax.jit
     def sampling_eval_function(tracerized_shots, *args):
         
-        return_amount = []
-        
+        # We now construct a loop to collect the samples by 
+        # inserting the postprocessed measurement result into an array.
+        # The following function is the loop body, which is kernelized.
         @quantum_kernel
         def sampling_body_func(i, args):
             
             acc = args[0]
             
+            # Evaluate the user function
             qv_tuple = user_func(*args[1:])
             
             if not isinstance(qv_tuple, tuple):
                 qv_tuple = (qv_tuple,)
             
+            # Save the return amount (for more details check the comment of the)
+            # initialization command of return_amount
             return_amount.append(len(qv_tuple))
             
             for qv in qv_tuple:
@@ -72,6 +104,8 @@ def sample(func = None, shots = 0, post_processor = None):
                     raise Exception("Tried to sample from function not returning a QuantumVariable")
             
             # Trace the DynamicQubitArray measurements
+            # Since we execute the measurements on the .reg attribute, no decoding
+            # is applied. The decoding happens in sampling_helper_2
             @qache
             def sampling_helper_1(*args):
                 res_list = []
@@ -92,6 +126,8 @@ def sample(func = None, shots = 0, post_processor = None):
                     decoded_values = jnp.array(post_processor(*decoded_values))
                 else:
                     decoded_values = post_processor(*decoded_values)
+                    
+                # Insert into the accumulating array
                 acc = acc.at[i].set(decoded_values)
                 
                 return acc
@@ -99,6 +135,15 @@ def sample(func = None, shots = 0, post_processor = None):
             acc = sampling_helper_2(acc, i, *measurement_ints)
             
             return (acc, *args[1:])
+        
+        # This list captures the amount of return values. The strategy here is
+        # to initially assume only one QuantumVariable is returned, which is then
+        # added to the expectation value accumulator. If more than one is returned,
+        # the amount is saved in this list and an exception is raised, which
+        # subsequently causes another call but this time with the correct accumulator
+        # dimension.
+        
+        return_amount = []
         
         try:
             loop_res = jax.lax.fori_loop(0, tracerized_shots, sampling_body_func, (jnp.zeros(shots), *args))
