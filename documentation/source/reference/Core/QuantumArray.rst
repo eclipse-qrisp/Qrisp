ccd2c91ec7c26ade822c173e9f8d1b7d6aa8b66a
positr0nium <raphaelsdel@gmail.com>
2025-03-12 15:07:13 +0100

    updated the documentation of QuantumArray

--- documentation/source/reference/Core/QuantumArray.rst
+++ documentation/source/reference/Core/QuantumArray.rst
@@ -17,8 +17,13 @@ Methods
    QuantumArray.most_likely
    QuantumArray.duplicate
 
-Array manipulation
-------------------
+Array Restructuring
+-------------------
+
+.. note::
+    
+    These methods never allocate additional qubits and instead return a 
+    `"view" <https://numpy.org/doc/2.2/user/basics.copies.html>`_.
 
 .. autosummary::
    :toctree: generated/
--- src/qrisp/core/gate_application_functions.py
+++ src/qrisp/core/gate_application_functions.py
@@ -159,7 +159,7 @@ def mcx(controls, target, method="auto", ctrl_state=-1, num_ancilla=1):
         *   - ``balauca``
             - Method based on this `paper <https://www.iccs-meeting.org/archive/iccs2022/papers/133530169.pdf>`__ with logarithmic depth but requires many ancilla qubits.
     	*   - ``khattar``
-            - Method based on this `paper <https://arxiv.org/abs/2407.17966>`_, implements the n-controlled x with 2n − 3 Toffoli and linear depth using 1 clean ancilla. The case n=3 implements this `paper <https://arxiv.org/abs/2106.11513>`_.
+            - Method based on this `paper <https://arxiv.org/abs/2407.17966>`_, implements the n-controlled x with 2n − 3 Toffoli and linear depth using 1 clean ancilla. The case n=3 implements this `paper <https://arxiv.org/abs/2106.11513>`__.
         *   - ``maslov``
             - Documented `here <https://arxiv.org/abs/1508.03273>`_, requires less ancilla qubits but is only available for 4 or less control qubits.
         *   - ``yong``
