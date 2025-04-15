b87c6255fd51c4e5288a660e7d0d79eab90aacf2
Rene Zander <rene.zander@fokus.fraunhofer.de>
2025-04-15 22:18:56 +0200

    Added test for jasp qaoa with tqa initialization

--- tests/qaoa_tests/test_jasp_QAOAmaxCut.py
+++ tests/qaoa_tests/test_jasp_QAOAmaxCut.py
@@ -42,4 +42,29 @@ def test_jasp_QAOAmaxCut():
     
     cut_value = main()
     assert cut_value < -4
+
+
+def test_jasp_tqa_QAOAmaxCut():
+    
+    @jaspify(terminal_sampling=True)
+    def main():
+
+        G = nx.erdos_renyi_graph(6, 0.7, seed = 133)
+
+        cl_cost = create_maxcut_sample_array_post_processor(G)
+
+        qarg = QuantumVariable(G.number_of_nodes())
+
+        qaoa_maxcut = QAOAProblem(cost_operator=create_maxcut_cost_operator(G),
+                            mixer=RX_mixer,
+                            cl_cost_function=cl_cost)
+        results = qaoa_maxcut.run(qarg, depth=10, max_iter=0, init_type='tqa', optimizer="SPSA")
+
+        cut_value = cl_cost(results)
+
+        return cut_value
+    
+    cut_value = main()
+    # When using tqa initialization, discretized quantum annealing is performed. Converges to optimal solution for increasing depth even for 0 iterations of the optimizer.
+    assert cut_value < -6
     
