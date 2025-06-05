088a592d92e75abd6500659e8fab6967979d5c7a
positr0nium <raphaelsdel@gmail.com>
2025-06-05 12:54:02 +0200

    improved the collection speed

--- src/qrisp/jasp/jasp_expression/environment_collection.py
+++ src/qrisp/jasp/jasp_expression/environment_collection.py
@@ -47,16 +47,17 @@ def collect_environments(jaxpr):
     # Once we hit an exit primitive, we collect the Equations between the enter
     # and exit primitive.
     eqn_list = list(jaxpr.eqns)
+    eqn_var_tracker = VarTracker(eqn_list)
     new_eqn_list = []
-
+    new_eqn_var_tracker = VarTracker([])
+    
     from qrisp.jasp import Jaspr
 
     if isinstance(jaxpr, Jaspr) and jaxpr.envs_flattened:
         return jaxpr
     
-    while len(eqn_list) != 0:
-
-        eqn = eqn_list.pop(0)
+    for j in range(len(eqn_list)):
+        eqn = eqn_list[j]
 
         if eqn.primitive.name == "pjit":
 
@@ -138,10 +139,12 @@ def collect_environments(jaxpr):
 
             # Set an alias for the equations marked as the body
             environment_body_eqn_list = new_eqn_list[i + 1 :]
-
+            environment_body_var_tracker = new_eqn_var_tracker.slice_start(i+1)
+            
+            invars = environment_body_var_tracker.find_invars()
+            
             # Remove the AbstractQuantumCircuit variable and prepend it.
             # invars = find_invars(environment_body_eqn_list)
-            invars = find_invars(environment_body_eqn_list)
             
             try:
                 invars.remove(enter_eq.outvars[0])
@@ -149,9 +152,9 @@ def collect_environments(jaxpr):
                 pass
 
             # Same for the outvars
-            outvars = find_outvars(
+            outvars = fast_find_outvars(
                 environment_body_eqn_list,
-                eqn_list,
+                eqn_var_tracker.slice_start(j+1),
                 [var for var in jaxpr.outvars if not isinstance(var, Literal)],
             )
 
@@ -175,9 +178,12 @@ def collect_environments(jaxpr):
 
             # Remove the collected equations from the new_eqn_list
             new_eqn_list = new_eqn_list[:i]
+            new_eqn_var_tracker = new_eqn_var_tracker.slice_end(i)
+            
 
         # Append the equation
         new_eqn_list.append(eqn)
+        new_eqn_var_tracker.append(eqn)
         
     if isinstance(jaxpr, Jaspr):
         res = jaxpr.update_eqns(new_eqn_list)
@@ -194,6 +200,7 @@ def collect_environments(jaxpr):
             outvars=jaxpr.outvars,
             eqns=new_eqn_list,
         )
+
 
 
 def find_invars(eqn_list):
@@ -234,57 +241,27 @@ def find_invars(eqn_list):
 
     return list(dict.fromkeys(invars))
 
-def find_invars(eqn_list):
-    
-    ind_to_var_dic = {}
-    var_to_ind_dic = {}
-    invar_indices = []
-    outvar_indices = []
-    
-    for eqn in eqn_list:
-        for var in eqn.invars:
-            try:
-                invar_indices.append(var_to_ind_dic[var])
-            except KeyError:
-                var_to_ind_dic[var] = len(var_to_ind_dic)
-                ind_to_var_dic[var_to_ind_dic[var]] = var
-                invar_indices.append(var_to_ind_dic[var])
-            except TypeError:
-                continue
-        for var in eqn.outvars:
-            try:
-                outvar_indices.append(var_to_ind_dic[var])
-            except KeyError:
-                var_to_ind_dic[var] = len(var_to_ind_dic)
-                ind_to_var_dic[var_to_ind_dic[var]] = var
-                outvar_indices.append(var_to_ind_dic[var])
-            except TypeError:
-                continue
-    
-    invar_index_list = jitted_kernel(invar_indices, outvar_indices)
-    
-    res = []
-    
-    for i in range(len(invar_index_list)):
-        res.append(ind_to_var_dic[invar_index_list[i]])
-    
-    res.sort(key = lambda x : var_to_ind_dic[x])
-    
-    return res
 
-def jitted_kernel(invar_indices, outvar_indices):
+def find_invar_kernel(invar_indices, outvar_indices):
     
-    max_var = max(invar_indices + outvar_indices, default = -1)
+    max_invar = np.max(invar_indices)
+    max_outvar = np.max(outvar_indices)
+    max_var = max(max_invar, max_outvar)
+    
     
     if max_var == -1:
-        return []
+        return np.zeros(0, dtype = np.int64)
     
-    invar_array = np.zeros(max_var+1, dtype = np.bool)
+    invar_array = np.zeros(max_var+1, dtype = np.int8)
     invar_array[invar_indices] = 1
     invar_array[outvar_indices] = 0
     res = np.nonzero(invar_array)[0]
     
     return res            
+
+from numba import njit
+
+jitted_find_invar_kernel = njit(find_invar_kernel)
 
 
 def find_outvars(body_eqn_list, script_remainder_eqn_list, return_vars):
@@ -321,3 +298,186 @@ def find_outvars(body_eqn_list, script_remainder_eqn_list, return_vars):
 
     # The result is the intersection between both sets of variables
     return list(set(outvars).intersection(required_remainder_vars + return_vars))
+
+
+def fast_find_outvars(body_eqn_list, script_remainder_var_tracker, return_vars):
+    """
+    This function takes the equations of a function and some "follow-up"
+    instructions and infers which variables need to be returned by the function.
+
+    Parameters
+    ----------
+    body_eqn_list : list[JaxprEqn]
+        A list of equations describing a function.
+    script_remainder_eqn_list : list[JaxprEqn]
+        A list of equations describing the follow up requirements.
+
+    Returns
+    -------
+    list[jax.core.Var]
+        A list of variables that would have to be returned by the function.
+
+    """
+
+    # This list will contain all variables produced by the function
+    outvars = []
+
+    # Fill the list
+    for eqn in body_eqn_list:
+        outvars.extend(eqn.outvars)
+
+    # Remove the duplicates
+    outvars = list(set(outvars))
+
+    # Find which variables are required for executing the follow-up
+    required_remainder_vars = script_remainder_var_tracker.find_invars()
+
+    # The result is the intersection between both sets of variables
+    return list(set(outvars).intersection(required_remainder_vars + return_vars))
+
+
+class VarTracker:
+    
+    def __init__(self, eqn_list):
+        
+        var_to_int_dic = {}
+        int_to_var_dic = {}
+        
+        eqn_invar_list = []
+        eqn_outvar_list = []
+        
+        outvar_eqn_index_tracker = [0]
+        invar_eqn_index_tracker = [0]
+        
+        for eqn in eqn_list:
+            invar_integers = []
+            for var in eqn.invars:
+                try:
+                    invar_integers.append(var_to_int_dic[var])
+                except KeyError:
+                    var_to_int_dic[var] = len(var_to_int_dic)
+                    int_to_var_dic[var_to_int_dic[var]] = var
+                    invar_integers.append(var_to_int_dic[var])
+                except TypeError:
+                    continue
+            outvar_integers = []
+            for var in eqn.outvars:
+                try:
+                    outvar_integers.append(var_to_int_dic[var])
+                except KeyError:
+                    var_to_int_dic[var] = len(var_to_int_dic)
+                    int_to_var_dic[var_to_int_dic[var]] = var
+                    outvar_integers.append(var_to_int_dic[var])
+                except TypeError:
+                    continue
+            
+            
+            eqn_invar_list.extend(invar_integers)
+            invar_eqn_index_tracker.append(len(eqn_invar_list))
+            
+            eqn_outvar_list.extend(outvar_integers)
+            outvar_eqn_index_tracker.append(len(eqn_outvar_list))
+            
+        
+        self.var_to_int_dic = var_to_int_dic
+        self.int_to_var_dic = int_to_var_dic
+        
+        self.eqn_invar_list = eqn_invar_list
+        self.eqn_outvar_list = eqn_outvar_list
+        
+        self.invar_eqn_index_tracker = invar_eqn_index_tracker
+        self.outvar_eqn_index_tracker = outvar_eqn_index_tracker
+        
+
+    def append(self, eqn):
+        
+        invar_integers = []
+        for var in eqn.invars:
+            try:
+                invar_integers.append(self.var_to_int_dic[var])
+            except KeyError:
+                self.var_to_int_dic[var] = len(self.var_to_int_dic)
+                self.int_to_var_dic[self.var_to_int_dic[var]] = var
+                invar_integers.append(self.var_to_int_dic[var])
+            except TypeError:
+                continue
+        outvar_integers = []
+        for var in eqn.outvars:
+            try:
+                outvar_integers.append(self.var_to_int_dic[var])
+            except KeyError:
+                self.var_to_int_dic[var] = len(self.var_to_int_dic)
+                self.int_to_var_dic[self.var_to_int_dic[var]] = var
+                outvar_integers.append(self.var_to_int_dic[var])
+            except TypeError:
+                continue
+    
+        
+        self.eqn_invar_list.extend(invar_integers)
+        self.invar_eqn_index_tracker.append(len(self.eqn_invar_list))
+        
+        self.eqn_outvar_list.extend(outvar_integers)
+        self.outvar_eqn_index_tracker.append(len(self.eqn_outvar_list))
+        
+        
+    def slice_start(self, starting_point):
+        
+        res = VarTracker([])
+        
+        invar_starting_point = self.invar_eqn_index_tracker[starting_point]
+        res.eqn_invar_list = self.eqn_invar_list[invar_starting_point:]
+        res.invar_eqn_index_tracker = [i - invar_starting_point for i in self.invar_eqn_index_tracker[starting_point:]]
+        
+        outvar_starting_point = self.outvar_eqn_index_tracker[starting_point]
+        res.eqn_outvar_list = self.eqn_outvar_list[outvar_starting_point:]
+        res.outvar_eqn_index_tracker = [i - outvar_starting_point for i in self.outvar_eqn_index_tracker[starting_point:]]
+        
+        res.int_to_var_dic = self.int_to_var_dic
+        res.var_to_int_dic = self.var_to_int_dic
+        
+        return res
+    
+    def slice_end(self, end_point):
+        
+        res = VarTracker([])
+        
+        invar_end_point = self.invar_eqn_index_tracker[end_point]
+        res.eqn_invar_list = self.eqn_invar_list[:invar_end_point]
+        res.invar_eqn_index_tracker = self.invar_eqn_index_tracker[:end_point+1]
+        
+        outvar_end_point = self.outvar_eqn_index_tracker[end_point]
+        res.eqn_outvar_list = self.eqn_outvar_list[:outvar_end_point]
+        res.outvar_eqn_index_tracker = self.outvar_eqn_index_tracker[:end_point+1]
+        
+        res.int_to_var_dic = self.int_to_var_dic
+        res.var_to_int_dic = self.var_to_int_dic
+        
+        return res
+    
+    def find_invars(self):
+        
+        if len(self.eqn_invar_list) < 20 or len(self.eqn_outvar_list) < 20:
+            invar_index_list = find_invar_kernel([-1] + self.eqn_invar_list, [-1] +self.eqn_outvar_list)
+        else:
+            invar_index_list = jitted_find_invar_kernel(np.array([-1] + self.eqn_invar_list, dtype = np.int32), 
+                                                        np.array([-1] +self.eqn_outvar_list, dtype = np.int32))
+            
+        
+        res = []
+        
+        
+        
+        for i in range(len(invar_index_list)):
+            res.append(self.int_to_var_dic[invar_index_list[i]])
+        
+        sorting_dic = {self.eqn_invar_list[i] : i for i in range(len(self.eqn_invar_list))[::-1]}
+        
+        res.sort(key = lambda x : sorting_dic[self.var_to_int_dic[x]])
+        
+        return res
+        
+        
+        
+        
+        
+        
