a1e157990183eb32ba8332e559d186de8ba990ac
positr0mium <raphaelsdel@gmail.com>
2025-07-14 16:03:40 +0200

    in-place multiplication also works with floats now

--- src/qrisp/alg_primitives/arithmetic/SBP_arithmetic.py
+++ src/qrisp/alg_primitives/arithmetic/SBP_arithmetic.py
@@ -1053,7 +1053,7 @@ def inpl_mult(qf, mult_int, treat_overflow=True):
 
     """
 
-    if not isinstance(mult_int, int):
+    if not isinstance(mult_int, (int, float)):
         raise Exception("Quantum inplace multiplication is restricted to classical values due to reversibility constraints")
 
     if mult_int < 0 and not qf.signed:
