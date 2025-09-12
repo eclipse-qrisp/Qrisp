8935bf5744f96acaa5bc7f066b43226b86ab7649
diehoq <diego.polimeni@epfl.ch>
2025-09-12 14:02:11 +0200

    Implement floor, ceil, round, fractional, modf for QuantumFloats

--- src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
+++ src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
@@ -20,6 +20,8 @@ from qrisp.qtypes import QuantumFloat
 from qrisp.environments import control, invert
 from qrisp.core.gate_application_functions import cx
 from qrisp.jasp import jrange 
+from typing import Tuple
+
 
 def qmax(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
 
@@ -63,3 +65,39 @@ def qmin(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
     c.delete()
             
     return res
+
+def qfloor(a: QuantumFloat) -> QuantumFloat:
+    b = a.duplicate()
+    for i in jrange(-a.exponent , a.size ):
+        cx(a[i],b[i])
+        
+    return b
+
+
+def qceil(a: QuantumFloat) -> QuantumFloat:
+    b = qfloor(a)
+    with control([a[i] for i in range(-a.exponent)]):
+        b+= 1   
+        
+    # b = -qfloor(-a) # LOOK AT THE r_inpl_sub
+    # b.extend(1) #DISCUSS THE OVERFLOW
+    return b
+
+
+def qround(a:QuantumFloat) -> QuantumFloat:
+    
+    
+    if not(a.signed):
+    # with not control(a.signed):
+        return qfloor(a + 0.5) ## USE check for tracing
+    if a.signed:
+    # with control(a.signed):
+        return qceil(a - 0.5)
+    
+def qfractional(a:QuantumFloat) -> QuantumFloat:
+    
+    b = a - qfloor(a)
+    return b
+
+def qmodf(a:QuantumFloat) -> Tuple[QuantumFloat, QuantumFloat]:
+    return (qfloor(a), qfractional(a))
