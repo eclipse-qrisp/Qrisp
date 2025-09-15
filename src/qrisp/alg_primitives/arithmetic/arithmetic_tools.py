3452aa8d36df1e297ec5069558cccd532583fd93
diehoq <diego.polimeni@epfl.ch>
2025-09-15 18:22:12 +0200

    Improve floor anc ceil, add logical OR, add doc

--- src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
+++ src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
@@ -15,40 +15,50 @@
 * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 ********************************************************************************
 """
-
+from qrisp.core import Qubit
 from qrisp.qtypes import QuantumFloat
 from qrisp.environments import control, invert
-from qrisp.core.gate_application_functions import cx
-from qrisp.jasp import jrange 
+from qrisp.core.gate_application_functions import x, cx, mcx
+from qrisp.jasp import jrange
 from typing import Tuple
 
 
 def qmax(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
+    """Returns a QuantumFloat containing the maximum of two QuantumFloats a and b
+    a: QuantumFloat
+    b: QuantumFloat
+    return: QuantumFloat
+    """
 
     res = QuantumFloat(a.size)
-    c = (a >= b)
-    
+    c = a >= b
+
     with control(c):
         for i in jrange(a.size):
             cx(a[i], res[i])
     with control(c, 0):
         for i in jrange(b.size):
             cx(b[i], res[i])
-    
-    compare_func = lambda x , y: x >= y
-    
+
+    compare_func = lambda x, y: x >= y
+
     with invert():
-        (c << compare_func)(a,b)
-    
+        (c << compare_func)(a, b)
+
     c.delete()
-            
+
     return res
 
 
 def qmin(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
 
+    """Returns a QuantumFloat containing the minimum of two QuantumFloats a and b
+    a: QuantumFloat
+    b: QuantumFloat
+    return: QuantumFloat
+    """
     res = QuantumFloat(a.size)
-    c = (a <= b)
+    c = a <= b
 
     with control(c):
         for i in jrange(a.size):
@@ -56,48 +66,87 @@ def qmin(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
     with control(c, 0):
         for i in jrange(b.size):
             cx(b[i], res[i])
-    
-    compare_func = lambda x , y: x <= y
-    
+
+    compare_func = lambda x, y: x <= y
+
     with invert():
-        (c << compare_func)(a,b)
-    
+        (c << compare_func)(a, b)
+
     c.delete()
-            
+
     return res
 
+
 def qfloor(a: QuantumFloat) -> QuantumFloat:
+    """Returns a QuantumFloat containing the floor of a QuantumFloat a
+    a: QuantumFloat
+    return: QuantumFloat
+    """
+    
     b = a.duplicate()
-    for i in jrange(-a.exponent , a.size ):
-        cx(a[i],b[i])
-        
+    for i in jrange(-a.exponent, a.size):
+        cx(a[i], b[i])
+
     return b
 
 
 def qceil(a: QuantumFloat) -> QuantumFloat:
+    """Returns a QuantumFloat containing the ceiling of a QuantumFloat a
+    a: QuantumFloat
+    return: QuantumFloat
+    """
     b = qfloor(a)
-    with control([a[i] for i in range(-a.exponent)]):
-        b+= 1   
-        
-    # b = -qfloor(-a) # LOOK AT THE r_inpl_sub
-    # b.extend(1) #DISCUSS THE OVERFLOW
+    
+    
+    #Logical OR on the fractional part
+    if a.exponent < 0:
+        flag = QuantumFloat(1)
+
+        Logical_OR([a[i] for i in range(-a.exponent)], flag)
+            
+        with control(flag):
+            b += 1
+
     return b
 
-
-def qround(a:QuantumFloat) -> QuantumFloat:
+def Logical_OR(operands: list[Qubit], flag: Qubit):
+    #handle the case fo an empty list
+    #Implement the case of a Quantum Variable with more the 2 qubits
+    for qb in operands:
+        x(qb)
+    x(flag)
+    mcx(operands, flag[0])
+    for qb in operands:
+        x(qb)
     
+def qround(a: QuantumFloat) -> QuantumFloat:
+    """Returns a QuantumFloat containing the rounded value of a QuantumFloat a
+    a: QuantumFloat
+    return: QuantumFloat
+    """
     
-    if not(a.signed):
-    # with not control(a.signed):
-        return qfloor(a + 0.5) ## USE check for tracing
     if a.signed:
-    # with control(a.signed):
-        return qceil(a - 0.5)
-    
-def qfractional(a:QuantumFloat) -> QuantumFloat:
-    
+        raise NotImplementedError("qround for signed QuantumFloat not implemented")
+        
+        # with control(a[-1]):
+        #     b = qceil(a - 0.5)
+        # b = qfloor(a + 0.5)
+        
+    return qfloor(a + 0.5)
+
+def qfractional(a: QuantumFloat) -> QuantumFloat:
+    """Returns a QuantumFloat containing the fractional part of a QuantumFloat a
+    a: QuantumFloat
+    return: QuantumFloat
+    """
+
     b = a - qfloor(a)
     return b
 
-def qmodf(a:QuantumFloat) -> Tuple[QuantumFloat, QuantumFloat]:
+
+def qmodf(a: QuantumFloat) -> Tuple[QuantumFloat, QuantumFloat]:
+    """Returns a tuple of two QuantumFloats containing the floor and fractional part of a QuantumFloat a
+    a: QuantumFloat
+    return: (QuantumFloat, QuantumFloat)
+    """
     return (qfloor(a), qfractional(a))
