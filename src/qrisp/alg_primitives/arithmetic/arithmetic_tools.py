1b06088704251287119787f84a57c4ecfe39f329
diehoq <diego.polimeni@epfl.ch>
2025-09-17 17:27:18 +0200

    Update qround and qfractional

--- src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
+++ src/qrisp/alg_primitives/arithmetic/arithmetic_tools.py
@@ -15,6 +15,7 @@
 * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 ********************************************************************************
 """
+
 from qrisp.core import Qubit
 from qrisp.qtypes import QuantumFloat
 from qrisp.environments import control, invert
@@ -51,7 +52,6 @@ def qmax(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
 
 
 def qmin(a: QuantumFloat, b: QuantumFloat) -> QuantumFloat:
-
     """Returns a QuantumFloat containing the minimum of two QuantumFloats a and b
     a: QuantumFloat
     b: QuantumFloat
@@ -82,7 +82,7 @@ def qfloor(a: QuantumFloat) -> QuantumFloat:
     a: QuantumFloat
     return: QuantumFloat
     """
-    
+
     b = a.duplicate()
     for i in jrange(-a.exponent, a.size):
         cx(a[i], b[i])
@@ -96,51 +96,82 @@ def qceil(a: QuantumFloat) -> QuantumFloat:
     return: QuantumFloat
     """
     b = qfloor(a)
-    
-    
-    #Logical OR on the fractional part
+
+    # Logical OR on the fractional part
     if a.exponent < 0:
         flag = QuantumFloat(1)
 
-        Logical_OR([a[i] for i in range(-a.exponent)], flag)
-            
+        logical_OR([a[i] for i in range(-a.exponent)], flag)
+
         with control(flag):
             b += 1
 
+        logical_OR([a[i] for i in range(-a.exponent)], flag)
+        flag.delete()
     return b
 
-def Logical_OR(operands: list[Qubit], flag: Qubit):
-    #handle the case fo an empty list
-    #Implement the case of a Quantum Variable with more the 2 qubits
+
+def logical_OR(operands: list[Qubit], flag: Qubit):
+    # handle the case fo an empty list
+    # Implement the case of a Quantum Variable with more the 2 qubits
     for qb in operands:
         x(qb)
     x(flag)
     mcx(operands, flag[0])
     for qb in operands:
         x(qb)
-    
+
+
 def qround(a: QuantumFloat) -> QuantumFloat:
     """Returns a QuantumFloat containing the rounded value of a QuantumFloat a
     a: QuantumFloat
     return: QuantumFloat
     """
-    
-    if a.signed:
-        raise NotImplementedError("qround for signed QuantumFloat not implemented")
-        
-        # with control(a[-1]):
-        #     b = qceil(a - 0.5)
-        # b = qfloor(a + 0.5)
-        
-    return qfloor(a + 0.5)
+    b = qfloor(a)
+
+    if a.exponent < 0:
+        with control(a[-a.exponent - 1]):
+            b += 1
+
+    return b
+
 
 def qfractional(a: QuantumFloat) -> QuantumFloat:
     """Returns a QuantumFloat containing the fractional part of a QuantumFloat a
     a: QuantumFloat
     return: QuantumFloat
     """
+    b = a.duplicate()
+    for i in jrange(a.size):
+        cx(a[i], b[i])
 
-    b = a - qfloor(a)
+    if a.signed:
+        flag = QuantumFloat(1)
+        cx(a[-1], flag)
+        with control(flag):
+            c = qceil(a)
+            b -= c
+            injected_qceil = c << qceil
+            with invert():
+                injected_qceil(a)
+            c.delete()
+            
+        with control(flag, 0):
+            c = qfloor(a)
+            b -= c
+            injected_qceil = c << qfloor
+            with invert():
+                injected_qceil(a)
+            c.delete()
+        cx(a[-1], flag)
+        flag.delete()
+    else:
+        c = qfloor(a)
+        b -= c
+        injected_qceil = c << qfloor
+        with invert():
+            injected_qceil(a)
+        c.delete()
     return b
 
 
