f0a012bc6b5adc48566cc8f2aa91f4a8a6cdd229
Purva Thakre <purva.athakre@gmail.com>
2025-11-07 13:42:50 +0100

    forgot phi in mcp docstring

--- src/qrisp/circuit/quantum_circuit.py
+++ src/qrisp/circuit/quantum_circuit.py
@@ -2198,6 +2198,8 @@ class QuantumCircuit:
 
         Parameters
         ----------
+        phi : float or sympy.Symbol
+            The angle parameter.
         control_qubits : list
             The list of Qubits to control on.
         target_qubits : Qubit
