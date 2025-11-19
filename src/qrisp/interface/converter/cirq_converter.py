e6b6d9b293f69f9d65c84ebe89d0331ba63e6914
Purva Thakre <purva.athakre@gmail.com>
2025-11-19 17:05:28 +0100

    ruff format

--- src/qrisp/interface/converter/cirq_converter.py
+++ src/qrisp/interface/converter/cirq_converter.py
@@ -3,43 +3,62 @@ import numpy as np
 from cirq import Circuit, LineQubit
 from qrisp.circuit import ControlledOperation
 
-from cirq import CNOT, H, X, Y, Z, S, T, SWAP, rx, ry, rz, inverse, I, M, R, CZ, CCNOT, ZPowGate, XPowGate
+from cirq import (
+    CNOT,
+    H,
+    X,
+    Y,
+    Z,
+    S,
+    T,
+    SWAP,
+    rx,
+    ry,
+    rz,
+    inverse,
+    I,
+    M,
+    R,
+    CZ,
+    CCNOT,
+    ZPowGate,
+    XPowGate,
+)
 
 qrisp_cirq_ops_dict = {
-    'cx': CNOT,
-    'cz': CZ,
-    'swap': SWAP,
-    '2cx': CCNOT,
-    'h': H,
-    'x': X,
-    'y': Y,
-    'z': Z,
-    'rx': rx,
-    'ry': ry,
-    'rz': rz,
-    's': S,
-    't': T,
-    's_dg': inverse(S),
-    't_dg': inverse(T),
-    'measure': M,
-    'reset': R,
-    'id': I,
-    'p': ZPowGate,
-    'sx': XPowGate,
-    'sx_dg': XPowGate,
+    "cx": CNOT,
+    "cz": CZ,
+    "swap": SWAP,
+    "2cx": CCNOT,
+    "h": H,
+    "x": X,
+    "y": Y,
+    "z": Z,
+    "rx": rx,
+    "ry": ry,
+    "rz": rz,
+    "s": S,
+    "t": T,
+    "s_dg": inverse(S),
+    "t_dg": inverse(T),
+    "measure": M,
+    "reset": R,
+    "id": I,
+    "p": ZPowGate,
+    "sx": XPowGate,
+    "sx_dg": XPowGate,
     # the converter skips adding the global phase gate
     # because the corresponding Cirq gate raises an error
     # for anything besides 1 and 1j as the coefficient
     # The qrisp global phase function does not accept
     # a complex value as input.
-    'gphase': None,
-    'xxyy': None,
-    'rxx': None,
-    'rzz': None,
-    'qb_alloc': None,
-    'qb_dealloc': None
+    "gphase": None,
+    "xxyy": None,
+    "rxx": None,
+    "rzz": None,
+    "qb_alloc": None,
+    "qb_dealloc": None,
 }
-
 
 
 def convert_to_cirq(qrisp_circuit):
@@ -49,10 +68,10 @@ def convert_to_cirq(qrisp_circuit):
     qrisp_circ_ops_data = qrisp_circuit.data
 
     # create generic 'ncx' keys in qrisp_cirq_ops_dict for the possibility of multicontrolled cx gates
-    for n in range(3, qrisp_circ_num_qubits+1):
+    for n in range(3, qrisp_circ_num_qubits + 1):
         # start at 3 because 2cx is a Toffoli gate which already exists in Cirq
-        qrisp_cirq_ops_dict[f'{n}cx'] = None
-    
+        qrisp_cirq_ops_dict[f"{n}cx"] = None
+
     # create an empty Cirq circuit
     cirq_circuit = Circuit()
     cirq_qubits = [LineQubit(i) for i in range(qrisp_circ_num_qubits)]
@@ -63,38 +82,44 @@ def convert_to_cirq(qrisp_circuit):
         qubit_map[q] = cirq_qubits[i]
 
     for instr in qrisp_circ_ops_data:
-        
-        # get the gate name, qubits it is acting on 
+        # get the gate name, qubits it is acting on
         # and parameters if there are any
         op_i = instr.op.name
         op_qubits_i = instr.qubits
-        
-        if hasattr(instr.op, 'params'):
-            params = instr.op.params
-        
-        if op_i not in qrisp_cirq_ops_dict:
-            raise ValueError(f"{op_i} gate is not supported by the Qrisp to Cirq converter.")
 
-        if op_i in [ 'gphase', 'rzz', 'rxx']:
-            print("Qrisp circuit contains a global phase gate which will be skipped in the Qrisp to Cirq conversion.")
-        
+        if hasattr(instr.op, "params"):
+            params = instr.op.params
+
+        if op_i not in qrisp_cirq_ops_dict:
+            raise ValueError(
+                f"{op_i} gate is not supported by the Qrisp to Cirq converter."
+            )
+
+        if op_i in ["gphase", "rzz", "rxx"]:
+            print(
+                "Qrisp circuit contains a global phase gate which will be skipped in the Qrisp to Cirq conversion."
+            )
+
         cirq_op_qubits = [qubit_map[q] for q in op_qubits_i]
 
         cirq_gate = qrisp_cirq_ops_dict[op_i]
-        
 
         # for single qubit parametrized gates
-        if op_i != 'id' and instr.op.params and not isinstance(instr.op, ControlledOperation):
+        if (
+            op_i != "id"
+            and instr.op.params
+            and not isinstance(instr.op, ControlledOperation)
+        ):
             # added op_i != 'id' becasue the identity gate has parameters (0, 0, 0)
-            if op_i == 'p':
+            if op_i == "p":
                 # Cirq does not have a phase gate
                 # for this reason, it has to be dealt with as a special case.
-                # the ZPowGate has a global phase in addition to the 
+                # the ZPowGate has a global phase in addition to the
                 # phase exponent. The default is to assume global_shift = 0 in cirq
                 exp_param = params[0]
                 cirq_circuit.append(ZPowGate(exponent=exp_param)(*cirq_op_qubits))
 
-            elif op_i == 'rxx':
+            elif op_i == "rxx":
                 phi = params[0]
                 # cirq_circuit.append(GlobalPhaseGate(-phi / 2).on())
                 cirq_circuit.append(H(cirq_op_qubits[0]))
@@ -105,48 +130,46 @@ def convert_to_cirq(qrisp_circuit):
                 cirq_circuit.append(H(cirq_op_qubits[0]))
                 cirq_circuit.append(H(cirq_op_qubits[1]))
 
-            elif op_i == 'rzz':
+            elif op_i == "rzz":
                 phi = params[0]
                 cirq_circuit.append(CNOT(cirq_op_qubits[0], cirq_op_qubits[1]))
                 cirq_circuit.append(ZPowGate(exponent=phi)(cirq_op_qubits[1]))
                 cirq_circuit.append(CNOT(cirq_op_qubits[0], cirq_op_qubits[1]))
 
-                
-            elif op_i =='xxyy':
+            elif op_i == "xxyy":
                 phi = params[0]
                 beta = params[1]
                 cirq_circuit.append(rz(beta).on(cirq_op_qubits[0]))
-                cirq_circuit.append(rz(-np.pi/2).on(cirq_op_qubits[1]))
+                cirq_circuit.append(rz(-np.pi / 2).on(cirq_op_qubits[1]))
                 cirq_circuit.append(XPowGate(exponent=0.5)(cirq_op_qubits[1]))
-                cirq_circuit.append(rz(np.pi/2).on(cirq_op_qubits[1]))
+                cirq_circuit.append(rz(np.pi / 2).on(cirq_op_qubits[1]))
                 cirq_circuit.append(S(cirq_op_qubits[0]))
                 cirq_circuit.append(CNOT(cirq_op_qubits[1], cirq_op_qubits[0]))
-                cirq_circuit.append(ry(-phi/2).on(cirq_op_qubits[1]))
-                cirq_circuit.append(ry(-phi/2).on(cirq_op_qubits[0]))
+                cirq_circuit.append(ry(-phi / 2).on(cirq_op_qubits[1]))
+                cirq_circuit.append(ry(-phi / 2).on(cirq_op_qubits[0]))
                 cirq_circuit.append(CNOT(cirq_op_qubits[1], cirq_op_qubits[0]))
                 cirq_circuit.append(inverse(S(cirq_op_qubits[0])))
-                cirq_circuit.append(rz(-np.pi/2).on(cirq_op_qubits[1]))
+                cirq_circuit.append(rz(-np.pi / 2).on(cirq_op_qubits[1]))
                 cirq_circuit.append(inverse(XPowGate(exponent=0.5)(cirq_op_qubits[1])))
-                cirq_circuit.append(rz(np.pi/2).on(cirq_op_qubits[1]))
+                cirq_circuit.append(rz(np.pi / 2).on(cirq_op_qubits[1]))
                 cirq_circuit.append(rz(-beta).on(cirq_op_qubits[0]))
 
             # elif op_i == 'gphase':
-                # global phase gate in Cirq cannot be applied to specific qubits
-                # cirq_circuit.append(GlobalPhaseGate(*params).on())
-                
+            # global phase gate in Cirq cannot be applied to specific qubits
+            # cirq_circuit.append(GlobalPhaseGate(*params).on())
+
             elif cirq_gate:
-                    gate_instance = cirq_gate(*params)
-                    cirq_circuit.append(gate_instance(*cirq_op_qubits))
-        
+                gate_instance = cirq_gate(*params)
+                cirq_circuit.append(gate_instance(*cirq_op_qubits))
+
         elif isinstance(instr.op, ControlledOperation):
             # control and target qubits from qrisp
-            control_qubits = instr.qubits[:len(instr.op.ctrl_state)]
-            target_qubits = instr.qubits[len(instr.op.ctrl_state):]
-            
+            control_qubits = instr.qubits[: len(instr.op.ctrl_state)]
+            target_qubits = instr.qubits[len(instr.op.ctrl_state) :]
 
             cirq_ctrl_qubits = [qubit_map[q] for q in control_qubits]
             cirq_target_qubits = [qubit_map[q] for q in target_qubits]
-            
+
             # verify the contorl and target qubit mapping worked
             assert cirq_op_qubits == cirq_ctrl_qubits + cirq_target_qubits
             assert op_qubits_i == control_qubits + target_qubits
@@ -156,21 +179,26 @@ def convert_to_cirq(qrisp_circuit):
                 # if the controlled operation is also parametrized
                 if instr.op.params:
                     gate_instance = cirq_gate(*params)
-                    cirq_circuit.append(gate_instance(*cirq_ctrl_qubits, *cirq_target_qubits))
+                    cirq_circuit.append(
+                        gate_instance(*cirq_ctrl_qubits, *cirq_target_qubits)
+                    )
                 else:
-                    cirq_circuit.append(cirq_gate(*cirq_ctrl_qubits, *cirq_target_qubits))
+                    cirq_circuit.append(
+                        cirq_gate(*cirq_ctrl_qubits, *cirq_target_qubits)
+                    )
             # for multi-qubit controlled operations mcx
             else:
-                cirq_circuit.append(X(*cirq_target_qubits).controlled_by(*cirq_ctrl_qubits))
-                
-        
+                cirq_circuit.append(
+                    X(*cirq_target_qubits).controlled_by(*cirq_ctrl_qubits)
+                )
+
         else:
             # for simple single qubit gates
-            if op_i == 'sx':
+            if op_i == "sx":
                 cirq_circuit.append(XPowGate(exponent=0.5)(*cirq_op_qubits))
-            elif op_i == 'sx_dg':
+            elif op_i == "sx_dg":
                 cirq_circuit.append(inverse(XPowGate(exponent=0.5)(*cirq_op_qubits)))
             elif cirq_gate:
                 cirq_circuit.append(cirq_gate(*cirq_op_qubits))
-    
+
     return cirq_circuit
--- tests/interface_tests/test_cirq_converter.py
+++ tests/interface_tests/test_cirq_converter.py
@@ -29,20 +29,20 @@ qc_single_qubit_gates.measure(0)
 qc_single_qubit_gates.reset(0)
 
 expected_cirq_qc_single_qubit_gates_ops = [
-        H(LineQubit(0)),
-        X(LineQubit(1)),
-        Y(LineQubit(3)),
-        Z(LineQubit(2)),
-        rx(rads=0.3).on(LineQubit(3)),
-        ry(rads=0.4).on(LineQubit(1)),
-        rz(rads=0.2).on(LineQubit(2)),
-        S(LineQubit(0)),
-        T(LineQubit(1)),
-        (T**-1).on(LineQubit(3)),
-        (S**-1).on(LineQubit(2)),
-        M(LineQubit(0)),
-        R(LineQubit(0))
-        ]
+    H(LineQubit(0)),
+    X(LineQubit(1)),
+    Y(LineQubit(3)),
+    Z(LineQubit(2)),
+    rx(rads=0.3).on(LineQubit(3)),
+    ry(rads=0.4).on(LineQubit(1)),
+    rz(rads=0.2).on(LineQubit(2)),
+    S(LineQubit(0)),
+    T(LineQubit(1)),
+    (T**-1).on(LineQubit(3)),
+    (S**-1).on(LineQubit(2)),
+    M(LineQubit(0)),
+    R(LineQubit(0)),
+]
 
 # 4 qubit circuit containing all two qubit gates
 
@@ -52,40 +52,72 @@ qc_mcx = QuantumCircuit(10)
 qc_mcx.mcx([0, 1, 2, 4, 5, 7], [3, 8])
 
 expected_mcx_circ = [
-    X(LineQubit(3)).controlled_by(LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(4), LineQubit(5), LineQubit(7)),
-    X(LineQubit(8)).controlled_by(LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(4), LineQubit(5), LineQubit(7))]
+    X(LineQubit(3)).controlled_by(
+        LineQubit(0),
+        LineQubit(1),
+        LineQubit(2),
+        LineQubit(4),
+        LineQubit(5),
+        LineQubit(7),
+    ),
+    X(LineQubit(8)).controlled_by(
+        LineQubit(0),
+        LineQubit(1),
+        LineQubit(2),
+        LineQubit(4),
+        LineQubit(5),
+        LineQubit(7),
+    ),
+]
 
-@pytest.mark.parametrize("input_circuit, expected_res", [
-    (qc_single_qubit_gates, expected_cirq_qc_single_qubit_gates_ops),
-    (qc_mcx, expected_mcx_circ)])
+
+@pytest.mark.parametrize(
+    "input_circuit, expected_res",
+    [
+        (qc_single_qubit_gates, expected_cirq_qc_single_qubit_gates_ops),
+        (qc_mcx, expected_mcx_circ),
+    ],
+)
 def test_cirq_converter(input_circuit, expected_res):
     """Check the Qrisp to Cirq converter works for different circuits."""
     converted_circ = convert_to_cirq(input_circuit)
     assert expected_res == list(converted_circ.all_operations())
 
 
-@pytest.mark.parametrize("op, expected_msg", [
-    (MagicMock(name='some_gate'), "some_gate gate is not supported by the Qrisp to Cirq converter."),
-    (MagicMock(spec=ClControlledOperation), "Classically controlled gate is not supported by the Qrisp to Cirq converter.")
-    ])
+@pytest.mark.parametrize(
+    "op, expected_msg",
+    [
+        (
+            MagicMock(name="some_gate"),
+            "some_gate gate is not supported by the Qrisp to Cirq converter.",
+        ),
+        (
+            MagicMock(spec=ClControlledOperation),
+            "Classically controlled gate is not supported by the Qrisp to Cirq converter.",
+        ),
+    ],
+)
 def test_unsupported_gate(op, expected_msg):
     """Mock unit test to verify an error is raised for an unsupported gate."""
     if isinstance(op, ClControlledOperation):
-        op.configure_mock(name='Classically controlled')
+        op.configure_mock(name="Classically controlled")
     else:
-        op.configure_mock(name='some_gate')
-    
+        op.configure_mock(name="some_gate")
+
     qc = QuantumCircuit(1)
     qc.data = [MagicMock(op=op, qubits=[MagicMock()])]
     with pytest.raises(ValueError, match=expected_msg):
         convert_to_cirq(qc)
-    
 
-@pytest.mark.parametrize("gate, qubits", [
-    ("rxx", [1, 3]),
-    ("rzz", [2, 0]),
-    ("gphase", [0]),
-])
+
+@pytest.mark.parametrize(
+    "gate, qubits",
+    [
+        ("rxx", [1, 3]),
+        ("rzz", [2, 0]),
+        ("gphase", [0]),
+    ],
+)
 def test_gphase_error(capsys, gate, qubits):
     """Verify a message is printed when the Qrisp circuit contains a global phase gate."""
     qc = QuantumCircuit(4)
@@ -98,7 +130,10 @@ def test_gphase_error(capsys, gate, qubits):
 
     convert_to_cirq(qc)
     captured = capsys.readouterr()
-    assert "Qrisp circuit contains a global phase gate which will be skipped in the Qrisp to Cirq conversion." in captured.out
+    assert (
+        "Qrisp circuit contains a global phase gate which will be skipped in the Qrisp to Cirq conversion."
+        in captured.out
+    )
 
 
 def test_converter_compiled_qs():
@@ -110,9 +145,13 @@ def test_converter_compiled_qs():
     mcx(ctrl, target)
     compiled_qc = ctrl.qs.compile()
     cirq_qc = convert_to_cirq(compiled_qc)
-    assert [X(LineQubit(4)).controlled_by(LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(3))] == list(cirq_qc.all_operations())
+    assert [
+        X(LineQubit(4)).controlled_by(
+            LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(3)
+        )
+    ] == list(cirq_qc.all_operations())
 
-    # reflection around GHZ state
+    # reflection around GHZ state in a QuantumSession
     # Prepare |1> state
     qv = QuantumVariable(5)
     x(qv)
@@ -137,7 +176,10 @@ def test_converter_compiled_qs():
         CNOT(LineQubit(0), LineQubit(2)),
         CNOT(LineQubit(0), LineQubit(3)),
         H(LineQubit(0)),
-        X(LineQubit(4)).controlled_by(LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(3)),
+        X(LineQubit(4)).controlled_by(
+            LineQubit(0), LineQubit(1), LineQubit(2), LineQubit(3)
+        ),
         H(LineQubit(4)),
         H(LineQubit(0)),
-        X(LineQubit(4))]
+        X(LineQubit(4)),
+    ]
