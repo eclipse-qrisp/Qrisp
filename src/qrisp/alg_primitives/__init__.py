481cca77f80ff90e0369b25193c73f2a621bb373
PietropaoloFrisoni <pietropfrisoni@gmail.com>
2025-11-25 12:44:59 +0100

    isort on init file

--- src/qrisp/alg_primitives/__init__.py
+++ src/qrisp/alg_primitives/__init__.py
@@ -16,18 +16,19 @@
 ********************************************************************************
 """
 
-from qrisp.alg_primitives.qft import *
-from qrisp.alg_primitives.qpe import *
-from qrisp.alg_primitives.qae import *
+from qrisp.alg_primitives.amplitude_amplification import *
+from qrisp.alg_primitives.arithmetic import *
+from qrisp.alg_primitives.dicke_state_prep import *
+from qrisp.alg_primitives.iterable_processing import *
 from qrisp.alg_primitives.iterative_qae import *
+from qrisp.alg_primitives.iterative_qpe import *
+from qrisp.alg_primitives.lcu import *
 from qrisp.alg_primitives.logic_synthesis import *
 from qrisp.alg_primitives.mcx_algs import *
-from qrisp.alg_primitives.arithmetic import *
-from qrisp.alg_primitives.iterable_processing import *
-from qrisp.alg_primitives.dicke_state_prep import *
-from qrisp.alg_primitives.switch_case import *
-from qrisp.alg_primitives.amplitude_amplification import *
-from qrisp.alg_primitives.lcu import *
 from qrisp.alg_primitives.prepare import *
-from qrisp.alg_primitives.iterative_qpe import *
+from qrisp.alg_primitives.qae import *
+from qrisp.alg_primitives.qft import *
+from qrisp.alg_primitives.qpe import *
 from qrisp.alg_primitives.reflection import *
+from qrisp.alg_primitives.state_preparation import *
+from qrisp.alg_primitives.switch_case import *
