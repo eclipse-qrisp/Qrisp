04379ad6fdf6ff73177714e405bfc656ca7aa507
Rene Zander <rene.zander@fokus.fraunhofer.de>
2026-01-28 14:25:49 +0100

    Moved GQET inversion to separate file; fixed some imports

--- src/qrisp/algorithms/gqsp/__init__.py
+++ src/qrisp/algorithms/gqsp/__init__.py
@@ -16,10 +16,10 @@
 ********************************************************************************
 """
 
-
 from qrisp.algorithms.gqsp.gqsp import *
 from qrisp.algorithms.gqsp.gqet import *
 from qrisp.algorithms.gqsp.convolution import *
+from qrisp.algorithms.gqsp.fourier_series_loader import *
 from qrisp.algorithms.gqsp.hamiltonian_simulation import *
 from qrisp.algorithms.gqsp.helper_functions import *
-from qrisp.algorithms.gqsp.fourier_series_loader import *
+from qrisp.algorithms.gqsp.inversion import *
