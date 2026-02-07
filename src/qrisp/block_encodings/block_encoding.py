Fixed scaling factor for chebyshev; added tests

src/qrisp/block_encodings/block_encoding.py
@@ -819,7 +819,7 @@ class BlockEncoding:
         -----
         - The Chebyshev polynomial approach is useful for polynomial approximations and spectral methods.
         - Should be used sparingly, primarily to combine a few block encodings. For larger-scale polynomial transformations, Quantum Signal Processing (QSP) is the superior method (see :meth:`poly`).
-        - **Normalization**: The resulting block-encoding maintains the same scaling factor $\alpha$ as the original.
+        - **Normalization**: If $k=1$, the resulting block-encoding maintains the same scaling factor $\alpha$ as the original. Otherwise, the scaling factor is $1$.
 
         Examples
         --------
@@ -849,6 +849,12 @@ class BlockEncoding:
             main(BE_cheb)
 
         """
+        
+        if rescale:
+            from qrisp.algorithms.gqsp.qet import QET
+            p = np.zeros(k+1)
+            p[-1] = 1.0
+            return QET(self, p, kind = "Chebyshev")
 
         m = len(self._anc_templates)
 
@@ -860,7 +866,6 @@ class BlockEncoding:
                     reflection(args[:m])
                     with conjugate(self.unitary)(*args):
                         reflection(args[:m])
-
         else:
             def new_unitary(*args):
                 for _ in jrange(0, iterations):
@@ -870,13 +875,8 @@ class BlockEncoding:
                 reflection(args[:m])
                 self.unitary(*args)
         
-        if rescale:
-            from qrisp.algorithms.gqsp.qet import QET
-            p = np.zeros(k+1)
-            p[-1] = 1.0
-            return QET(self, p, kind = "Chebyshev")
-           
-        return BlockEncoding(self.alpha, self._anc_templates, new_unitary)
+        new_alpha = self.alpha if k==1 else 1
+        return BlockEncoding(new_alpha, self._anc_templates, new_unitary)
 
     #
     # Arithmetic

tests/block_encodings_tests/test_block_encoding_arithmetic.py
@@ -17,10 +17,16 @@
 """
 
 import numpy as np
-import jax.numpy as jnp
 import pytest
 from qrisp import *
 from qrisp.operators import X, Y, Z
+
+
+def compare_results(res_dict_1, res_dict_2, n):
+    for k in range(2 ** n):
+        val_1 = res_dict_1.get(k, 0)
+        val_2 = res_dict_2.get(k, 0)
+        assert np.isclose(val_1, val_2), f"Mismatch at state |{k}>: {val_1} vs {val_2}"
 
 
 @pytest.mark.parametrize("H1, H2", [
@@ -45,11 +51,7 @@ def test_block_encoding_addition(H1, H2):
 
     res_be3 = main(BE3)
     res_be_add = main(BE_addition)
-
-    for k in range(2 ** n):
-        val_be3 = res_be3.get(k, 0)
-        val_be_add = res_be_add.get(k, 0)
-        assert np.isclose(val_be3, val_be_add), f"Mismatch at state |{k}>: {val_be3} vs {val_be_add}"
+    compare_results(res_be3, res_be_add, n)
 
 
 @pytest.mark.parametrize("H1, H2", [
@@ -74,11 +76,7 @@ def test_block_encoding_subtraction(H1, H2):
     
     res_be3 = main(BE3)
     res_be_sub = main(BE_subtraction)
-
-    for k in range(2 ** n):
-        val_be3 = res_be3.get(k, 0)
-        val_be_sub = res_be_sub.get(k, 0)
-        assert np.isclose(val_be3, val_be_sub), f"Mismatch at state |{k}>: {val_be3} vs {val_be_sub}"
+    compare_results(res_be3, res_be_sub, n)
 
 
 # The product of two Hermitian operators A and B is Hermitian if and only if they commute, i.e., AB = BA.
@@ -105,11 +103,7 @@ def test_block_encoding_multiplication(H1, H2):
     
     res_be3 = main(BE3)
     res_be_mul = main(BE_multiplication)
-
-    for k in range(2 ** n):
-        val_be3 = res_be3.get(k, 0)
-        val_be_mul = res_be_mul.get(k, 0)
-        assert np.isclose(val_be3, val_be_mul), f"Mismatch at state |{k}>: {val_be3} vs {val_be_mul}"
+    compare_results(res_be3, res_be_mul, n)
 
 
 @pytest.mark.parametrize("H1, H2, scalar", [
@@ -136,14 +130,8 @@ def test_block_encoding_scalar_multiplication(H1, H2, scalar):
     res_target = main(BE_target)
     res_left = main(BE_left)
     res_right = main(BE_right)
-
-    for k in range(2 ** n):
-        val_target = res_target.get(k, 0)
-        val_left = res_left.get(k, 0)
-        val_right = res_right.get(k, 0)
-        
-        assert np.isclose(val_target, val_left), f"Left-mul mismatch at |{k}>"
-        assert np.isclose(val_target, val_right), f"Right-mul mismatch at |{k}>"
+    compare_results(res_target, res_left, n)
+    compare_results(res_target, res_right, n)
 
 
 @pytest.mark.parametrize("H1, H2", [
@@ -164,13 +152,9 @@ def test_block_encoding_negation(H1, H2):
     def main(BE):
         return BE.apply_rus(lambda: QuantumVariable(n))()
     
-    res_be_neg = main(BE_neg)
     res_be2 = main(BE2)
-
-    for k in range(2 ** n):
-        val_be_neg = res_be_neg.get(k, 0)
-        val_be2 = res_be2.get(k, 0)
-        assert np.isclose(val_be_neg, val_be2), f"Mismatch at state |{k}>: {val_be_neg} vs {val_be2}"
+    res_be_neg = main(BE_neg)
+    compare_results(res_be2, res_be_neg, n)
 
 
 @pytest.mark.parametrize("H1, H2", [