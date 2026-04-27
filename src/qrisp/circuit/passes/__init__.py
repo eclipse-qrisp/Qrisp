f89f85159b7537b9d07b6b11d1f25d35111f5e55
positr0nium <raphaelsdel@gmail.com>
2026-04-27 22:14:16 +0200

    implemented convert_to_cz pass

--- src/qrisp/circuit/passes/__init__.py
+++ src/qrisp/circuit/passes/__init__.py
@@ -20,5 +20,6 @@ from qrisp.circuit.passes.pass_manager import PassManager
 from qrisp.circuit.passes.arange_swaps import arange_swaps
 from qrisp.circuit.passes.cancel_zero_controls import cancel_zero_controls
 from qrisp.circuit.passes.commute_swaps import commute_swaps
+from qrisp.circuit.passes.convert_to_cz import convert_to_cz
 
-__all__ = ["PassManager", "arange_swaps", "cancel_zero_controls", "commute_swaps"]
+__all__ = ["PassManager", "arange_swaps", "cancel_zero_controls", "commute_swaps", "convert_to_cz"]
