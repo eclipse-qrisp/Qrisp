3ca85d1d3240ca9ac4c4c0f85e6e1924a9ffd48b
mat70593 <matic.petric@fokus.fraunhofer.de>
2026-06-02 15:43:05 +0200

    implemented QSVT, yet to be included in other functions withing the GQSP folder/algorithm functionalities

--- src/qrisp/algorithms/gqsp/qsvt.py
+++ src/qrisp/algorithms/gqsp/qsvt.py
@@ -15,3 +15,80 @@
 * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 ********************************************************************************
 """
+from qrisp import (
+    QuantumBool,
+    invert,
+    rz,
+    h,
+    mcx,
+)
+from qrisp.environments import conjugate, control
+from qrisp.algorithms.gqsp.gqsp_angles import qsvt_angles
+from qrisp.algorithms.gqsp.helper_functions import poly2cheb, _rescale_poly
+from qrisp.block_encodings import BlockEncoding
+from qrisp.jasp import jrange, q_cond
+from qrisp.operators import QubitOperator, FermionicOperator
+from jax import numpy as jnp
+from typing import Literal, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from jax.typing import ArrayLike
+
+def QSVT(
+    H: BlockEncoding | FermionicOperator | QubitOperator,
+    p: "ArrayLike",
+    kind: Literal["Polynomial", "Chebyshev"] = "Polynomial",
+    parity: Literal["odd", "even"] = "odd",
+    rescale: bool = True,
+) -> BlockEncoding:
+    
+    ALLOWED_KINDS = {"Polynomial", "Chebyshev"}
+    if kind not in ALLOWED_KINDS:
+        raise ValueError(
+            f"Invalid kind specified: '{kind}'. "
+            f"Allowed kinds are: {', '.join(ALLOWED_KINDS)}"
+        )
+    
+    if isinstance(H, (QubitOperator, FermionicOperator)):
+        H = BlockEncoding.from_operator(H)
+    
+    # Rescaling of the polynomial to account for scaling factor alpha of block-encoding
+    if rescale:
+        p = _rescale_poly(H.alpha, p, kind=kind)
+    if kind == "Polynomial":
+        p = poly2cheb(p)
+    
+    phi, alpha = qsvt_angles(p)
+        
+    print(phi)
+    
+    m = len(H._anc_templates)
+
+    def reflection(args, phase):
+        qubits = sum([arg.reg for arg in args[1:m + 1]], []) 
+        with conjugate(mcx)(qubits, args[0], ctrl_state=0):
+            rz(phase, args[0])
+
+    def even(args):
+        H.unitary(*args[1:])
+
+    def odd(args):
+        with invert():
+            H.unitary(*args[1:])
+
+    def new_unitary(*args):
+        h(args[0]) 
+
+        d = len(phi) - 1
+                
+        for i in jrange(0, d): 
+            reflection(args, phase = 2 * phi[d-i])
+            q_cond(i%2==0, even, odd, args) 
+        reflection(args, phase=2 * phi[0])
+            
+        h(args[0])
+
+    new_anc_templates = [QuantumBool().template()] + H._anc_templates
+    new_alpha = alpha
+
+    return BlockEncoding(new_alpha, new_anc_templates, new_unitary, is_hermitian = False)
