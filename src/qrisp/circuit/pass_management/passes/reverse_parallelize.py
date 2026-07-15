5dea85ed9161f7cd09edf423914fcd0dc5b01ae9
positr0nium <raphaelsdel@gmail.com>
2026-07-15 11:34:50 +0200

    fixed wrong license header

--- src/qrisp/circuit/pass_management/passes/reverse_parallelize.py
+++ src/qrisp/circuit/pass_management/passes/reverse_parallelize.py
@@ -1,14 +1,19 @@
-# This work is licensed under the European Union Public License (EUPL), Version 1.2.
-#
-# You may not use this work except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-# https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
-#
-# Unless required by applicable law or agreed to in writing, software distributed under
-# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific language
-# governing permissions and limitations under the License.
+"""********************************************************************************
+* Copyright (c) 2026 the Qrisp authors
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* This Source Code may also be made available under the following Secondary
+* Licenses when the conditions for such availability set forth in the Eclipse
+* Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+* with the GNU Classpath Exception which is
+* available at https://www.gnu.org/software/classpath/license.html.
+*
+* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+********************************************************************************
+"""
 
 """Reverse-order parallelization pass.
 
