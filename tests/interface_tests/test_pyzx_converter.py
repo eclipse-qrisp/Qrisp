45bf2b5e765e5f8d88155f3d0fa920c72c2ca84f
philipp-heinen <philippheinen97@gmail.com>
2026-08-05 22:08:58 +0200

    Ran ruff format on test_pyzx_converter.py

--- tests/interface_tests/test_pyzx_converter.py
+++ tests/interface_tests/test_pyzx_converter.py
@@ -4,6 +4,7 @@ from fractions import Fraction
 
 import numpy as np
 
+
 def _build_single_qubit_qrisp_circuit():
     """Qrisp circuit with single-qubit gates.
     Example adapted from cirq converter test"""
@@ -23,38 +24,41 @@ def _build_single_qubit_qrisp_circuit():
     qc.t_dg(3)
     qc.s_dg(2)
     qc.sx_dg(3)
-    qc.gphase(0.5,0)
+    qc.gphase(0.5, 0)
     qc.id(0)
     return qc
+
 
 def _build_multi_qubit_qrisp_circuit():
     """Qrisp circuit with multi-qubit gates."""
     qc = QuantumCircuit(4)
-    qc.cx(0,1)
-    qc.cy(2,3)
-    qc.cz(0,2)
+    qc.cx(0, 1)
+    qc.cy(2, 3)
+    qc.cz(0, 2)
     qc.swap(2, 3)
     qc.xxyy(0, 1, 2, 3)
     qc.rxx(0.1, 0, 3)
     qc.rzz(0.2, 1, 2)
     return qc
 
+
 def _build_single_qubit_pyzx_circuit():
     """PyZX circuit with single-qubit gates."""
     c = Circuit(4)
     c.add_gate("NOT", 0)
     c.add_gate("Y", 1)
-    c.add_gate("Z", 2),
+    (c.add_gate("Z", 2),)
     c.add_gate("HAD", 3)
-    c.add_gate("XPhase", 0, Fraction(2,3))
-    c.add_gate("YPhase", 1, Fraction(1,3))
-    c.add_gate("ZPhase", 2, Fraction(1,6))
-    c.add_gate("U2", 3, Fraction(2,7), Fraction(9,8))
-    c.add_gate("U3", 0, Fraction(6,7), Fraction(3,2), Fraction(5,4))
+    c.add_gate("XPhase", 0, Fraction(2, 3))
+    c.add_gate("YPhase", 1, Fraction(1, 3))
+    c.add_gate("ZPhase", 2, Fraction(1, 6))
+    c.add_gate("U2", 3, Fraction(2, 7), Fraction(9, 8))
+    c.add_gate("U3", 0, Fraction(6, 7), Fraction(3, 2), Fraction(5, 4))
     c.add_gate("SX", 1)
     c.add_gate("S", 2)
     c.add_gate("T", 3)
     return c
+
 
 def _build_multi_qubit_pyzx_circuit():
     """PyZX circuit with multi-qubit gates."""
@@ -62,53 +66,52 @@ def _build_multi_qubit_pyzx_circuit():
     c.add_gate("CNOT", 0, 1)
     c.add_gate("CY", 2, 3)
     c.add_gate("CZ", 1, 2)
-    c.add_gate("CRX", 0, 2, Fraction(2,3))
-    c.add_gate("CRY", 0, 2, Fraction(4,3))
-    c.add_gate("CRZ", 0, 2, Fraction(1,2))
-    c.add_gate("CPhase", 0, 1, Fraction(4,5))
-    c.add_gate("ParityPhase", Fraction(6,5), 0, 2, 3)
+    c.add_gate("CRX", 0, 2, Fraction(2, 3))
+    c.add_gate("CRY", 0, 2, Fraction(4, 3))
+    c.add_gate("CRZ", 0, 2, Fraction(1, 2))
+    c.add_gate("CPhase", 0, 1, Fraction(4, 5))
+    c.add_gate("ParityPhase", Fraction(6, 5), 0, 2, 3)
     c.add_gate("XCX", 0, 1)
-    #SX gate has a different phase definition in PyZX, hence CSX gives a unitary that is equivalent, but does not only differ in a global phase
-    #leave out for now
-    #c.add_gate("CSX", 0, 1)
+    # SX gate has a different phase definition in PyZX, hence CSX gives a unitary that is equivalent, but does not only differ in a global phase
+    # leave out for now
+    # c.add_gate("CSX", 0, 1)
     c.add_gate("SWAP", 0, 2)
     c.add_gate("CSWAP", 0, 1, 3)
     c.add_gate("CHAD", 0, 1)
     c.add_gate("TOF", 1, 2, 3)
     c.add_gate("CCZ", 0, 2, 3)
-    c.add_gate("CU3", 0, 1, Fraction(6,5), Fraction(3,2), Fraction(7,4))
-    c.add_gate("RZZ", 0, 2, Fraction(2,9))
-    c.add_gate("RXX", 0, 2, Fraction(7,10))
+    c.add_gate("CU3", 0, 1, Fraction(6, 5), Fraction(3, 2), Fraction(7, 4))
+    c.add_gate("RZZ", 0, 2, Fraction(2, 9))
+    c.add_gate("RXX", 0, 2, Fraction(7, 10))
 
     return c
 
+
 def _compare_unitaries(U1, U2):
-    #PyZX does not keep track of global phases and instead normalizes the phase,
-    #which leads to a potential phase mismatch.
-    #We determine the phase difference by dividing the matrix entries with the largest magnitude
-    ind_max = np.unravel_index(
-            np.argmax(np.abs(U1)),
-            U1.shape
-        )
+    # PyZX does not keep track of global phases and instead normalizes the phase,
+    # which leads to a potential phase mismatch.
+    # We determine the phase difference by dividing the matrix entries with the largest magnitude
+    ind_max = np.unravel_index(np.argmax(np.abs(U1)), U1.shape)
     phase = U2[ind_max] / U1[ind_max]
 
-    np.testing.assert_array_almost_equal(
-        phase * U1,
-        U2
-    )
+    np.testing.assert_array_almost_equal(phase * U1, U2)
+
 
 def _test_qrisp_to_pyzx(qc):
     c = qc.to_pyzx()
     _compare_unitaries(qc.get_unitary(), c.to_matrix())
 
+
 def _test_pyzx_to_qrisp(c):
     qc = QuantumCircuit.from_pyzx(c)
     _compare_unitaries(qc.get_unitary(), c.to_matrix())
+
 
 def _test_roundtrip(qc):
     c = qc.to_pyzx()
     qc2 = QuantumCircuit.from_pyzx(c)
     _compare_unitaries(qc.get_unitary(), qc2.get_unitary())
+
 
 def _test_roundtrip_reverse(c):
     qc = QuantumCircuit.from_pyzx(c)
@@ -116,10 +119,10 @@ def _test_roundtrip_reverse(c):
     _compare_unitaries(c.to_matrix(), c2.to_matrix())
 
 
-
 def test_single_qubit_circuit_qrisp_to_pyzx():
     qc = _build_single_qubit_qrisp_circuit()
     _test_qrisp_to_pyzx(qc)
+
 
 def test_multi_qubit_circuit_qrisp_to_pyzx():
     qc = _build_multi_qubit_qrisp_circuit()
@@ -130,6 +133,7 @@ def test_single_qubit_circuit_pyzx_to_qrisp():
     c = _build_single_qubit_pyzx_circuit()
     _test_pyzx_to_qrisp(c)
 
+
 def test_multi_qubit_circuit_pyzx_to_qrisp():
     c = _build_multi_qubit_pyzx_circuit()
     _test_pyzx_to_qrisp(c)
@@ -138,6 +142,7 @@ def test_multi_qubit_circuit_pyzx_to_qrisp():
 def test_single_qubit_circuit_roundtrip():
     qc = _build_single_qubit_qrisp_circuit()
     _test_roundtrip(qc)
+
 
 def test_multi_qubit_circuit_roundtrip():
     qc = _build_multi_qubit_qrisp_circuit()
@@ -148,6 +153,7 @@ def test_single_qubit_circuit_roundtrip_reverse():
     c = _build_single_qubit_pyzx_circuit()
     _test_roundtrip_reverse(c)
 
+
 def test_multi_qubit_circuit_roundtrip_reverse():
     c = _build_multi_qubit_pyzx_circuit()
     _test_roundtrip_reverse(c)
@@ -157,6 +163,7 @@ def test_qrisp_transpilation():
     """Test transpilation capability of converter for a circuit that has to be transpiled.
     Example taken from test_cirq_converter.py"""
     from qrisp import p, h, QPE
+
     def U(qv):
         x = 0.5
         y = 0.125
@@ -168,7 +175,7 @@ def test_qrisp_transpilation():
     h(qv)
     QPE(qv, U, precision=3)
     qc = qv.qs.compile()
-    
+
     _test_qrisp_to_pyzx(qc)
     _test_roundtrip(qc)
 
@@ -179,8 +186,8 @@ def test_non_unitaries():
     qc.measure(0)
     qc.reset(1)
     c = qc.to_pyzx()
-    assert [g.name for g in c.gates] == ['Measurement', 'Reset']
-    assert [g.target for g in c.gates] == [0,1]
+    assert [g.name for g in c.gates] == ["Measurement", "Reset"]
+    assert [g.target for g in c.gates] == [0, 1]
 
     c = Circuit(2)
     c.add_gate("Measurement", 0)
