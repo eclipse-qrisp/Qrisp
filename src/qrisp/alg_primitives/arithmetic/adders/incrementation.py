be7263e3bf6c125f75c19ee65b280c741d5da825
Purva Thakre <purva.athakre@gmail.com>
2026-08-24 20:51:31 +0200

    test expected failure on both linter/formatter

--- src/qrisp/alg_primitives/arithmetic/adders/incrementation.py
+++ src/qrisp/alg_primitives/arithmetic/adders/incrementation.py
@@ -234,3 +234,9 @@ def lin_incr(a, c_in=None, c_out=None):
         cx(c_in, a[0])
 
     incr_anc.delete()
+
+
+def test_adder_helpers():
+    test_values=[1,2,3]
+    unused = 42
+    return test_values
