b8164ed1606ffd2c886647dd3f01137a1c5dc0c9
Jirka Guth Jarkovsky <85372852+JiriGuthJarkovsky@users.noreply.github.com>
2026-08-25 14:49:15 +0200

    Add noqa comment

--- src/qrisp/alg_primitives/dicke_state_prep.py
+++ src/qrisp/alg_primitives/dicke_state_prep.py
@@ -308,7 +308,7 @@ def _split_cycle_shift(qv: QuantumVariable | Sequence[Qubit], n: int | Array, k:
     cx(qv[n - 2], qv[n - 1])
 
     # 2 <= l <= k
-    for l in jrange(2, k + 1):
+    for l in jrange(2, k + 1):  # noqa: E741
         param = 2 * jnp.arccos(jnp.sqrt(l / n))
 
         cx(qv[n - l - 1], qv[n - 1])
