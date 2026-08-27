220a199af93e66448351c9678f706925a33ac1b5
Purva Thakre <purva.athakre@gmail.com>
2026-08-27 10:52:37 +0200

    delete test file

--- src/qrisp/_reviewdog_type_check_test.py
+++
@@ -1,6 +0,0 @@
-"""Temporary test to verify the mypy reviewdog check passes as expected."""
-
-
-def _type_check_ok(x: int) -> str:
-    """Temporary helper used to verify the mypy reviewdog check passes."""
-    return str(x)
