474f835db4db27b0fac3c999bdb79b8ac398ba1e
renezander90 <rene.zander@fokus.fraunhofer.de>
2026-09-26 17:57:08 +0200

    Fix license headers and formatting

--- tests/primitives_tests/ntt_tests/test_ntt_tools.py
+++ tests/primitives_tests/ntt_tests/test_ntt_tools.py
@@ -1,19 +1,20 @@
-"""********************************************************************************
-* Copyright (c) 2026 the Qrisp authors
-*
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License 2.0 which is available at
-* http://www.eclipse.org/legal/epl-2.0.
-*
-* This Source Code may also be made available under the following Secondary
-* Licenses when the conditions for such availability set forth in the Eclipse
-* Public License, v. 2.0 are satisfied: GNU General Public License, version 2
-* with the GNU Classpath Exception which is
-* available at https://www.gnu.org/software/classpath/license.html.
-*
-* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
-********************************************************************************
-"""
+# ********************************************************************************
+# * Copyright (c) 2026 the Qrisp authors
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * This Source Code may also be made available under the following Secondary
+# * Licenses when the conditions for such availability set forth in the Eclipse
+# * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+# * with the GNU Classpath Exception which is
+# * available at https://www.gnu.org/software/classpath/license.html.
+# *
+# * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+# ********************************************************************************
+
+"""Tests for the quantum Number Theoretic Transform (qNTT) and its related tools."""
 
 import numpy as np
 import jax.numpy as jnp
@@ -71,9 +72,7 @@ def test_ntt(a, n, q, root):
     ],
 )
 def test_qc_multiply_ntts(a, b, n, q, root):
-    """
-    Tests that quantum-classical multiplication of NTTs matches classical multiplication of NTTs.
-    """
+    """Tests that quantum-classical multiplication of NTTs matches classical multiplication of NTTs."""
 
     @boolean_simulation
     def main():
